aae27d7955c7330e3de9ae3513a97f028a83fb0d
stoyanov7 <georgi.stoyanov777@gmail.com>
2016-11-17 18:02:18 +0200

    Create 11.EqualWords.java

---
+++ .ProgramingBasics/.SimpleConditionalStatements/11.EqualWords.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input1 = scanner.nextLine().toLowerCase();
+        String input2 = scanner.nextLine().toLowerCase();
+
+        if (input1.equals(input2)) {
+            System.out.println("yes");
+        }
+        else {
+            System.out.println("no");
+        }
+    }
+}
