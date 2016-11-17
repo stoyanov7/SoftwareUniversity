db9bde062ee93b0f12ea915dfdd817ec8e60b478
stoyanov7 <georgi.stoyanov777@gmail.com>
2016-11-17 22:09:34 +0200

    Create 04.TriangleOfDollars.java

---
+++ .ProgramingBasics/DrawingFiguresWithLoops/04.TriangleOfDollars.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(repeatStr("$ ", i));
+        }
+    }
+
+    private static String repeatStr(String str, int count) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            sb.append(str);
+        }
+        return sb.toString();
+    }
+}
