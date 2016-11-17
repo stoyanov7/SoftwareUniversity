9b5dfe2db044e6187227f814d8066fb985a512e9
stoyanov7 <georgi.stoyanov777@gmail.com>
2016-11-17 17:47:18 +0200

    Create 01.Excellent Result.java

---
+++ .ProgramingBasics/.SimpleConditionalStatements/01.Excellent Result.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double grade = scanner.nextDouble();
+
+        if (grade >= 5.50) {
+            System.out.println("Excellent!");
+        }
+    }
+}
