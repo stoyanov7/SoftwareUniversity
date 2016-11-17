b500295afa7c0aac83712a839f47e0e8d46674ac
stoyanov7 <georgi.stoyanov777@gmail.com>
2016-11-17 22:15:52 +0200

    Create 01.Numbers1...NВWithStep3.java

---
+++ .ProgramingBasics/AdvancedLoops/01.Numbers1...NВWithStep3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 1; i <= n; i += 3) {
+            System.out.println(i);
+        }
+    }
+}
