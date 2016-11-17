d69e23a6f3eb9ed316b8f05245073cfb49eeeb21
Georgi <georgi.stoyanov777@gmail.com>
2016-11-17 23:41:24 +0200

    Create 05.IntegerТoHexАndBinary.java

---
+++ Java-Basics/05.IntegerТoHexАndBinary.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+        System.out.println(Integer.toHexString(input).toUpperCase());
+        System.out.println(Integer.toBinaryString(input));
+    }
+}
