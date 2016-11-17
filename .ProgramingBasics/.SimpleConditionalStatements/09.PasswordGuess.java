d63ab2f3c5ef33eecd41ed5d1df2aa5529cd4ee7
stoyanov7 <georgi.stoyanov777@gmail.com>
2016-11-17 17:58:25 +0200

    Create 09.PasswordGuess.java

---
+++ .ProgramingBasics/.SimpleConditionalStatements/09.PasswordGuess.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final String password = "s3cr3t!P@ssw0rd";
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        if (input.equals(password)) {
+            System.out.println("Welcome");
+        }
+        else {
+            System.out.println("Wrong password!");
+        }
+    }
+}
