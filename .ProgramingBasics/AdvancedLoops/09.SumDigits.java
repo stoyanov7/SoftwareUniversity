92f2c737c74b31f073f08313dee87129734cecf5
stoyanov7 <georgi.stoyanov777@gmail.com>
2016-11-17 22:33:42 +0200

    Create 09.SumDigits.java

---
+++ .ProgramingBasics/AdvancedLoops/09.SumDigits.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+
+        do {
+            sum += (n % 10);
+            n /= 10;
+        }while (n > 0);
+
+        System.out.println(sum);
+    }
+}
