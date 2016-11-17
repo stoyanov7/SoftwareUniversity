3ef1078fd8d3f05bdfd333737446fbc714f7a1d9
stoyanov7 <georgi.stoyanov777@gmail.com>
2016-11-17 22:30:05 +0200

    Create 07.GreatestCommonDivisor.java

---
+++ .ProgramingBasics/AdvancedLoops/07.GreatestCommonDivisor.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        System.out.println(a);
+    }
+}
