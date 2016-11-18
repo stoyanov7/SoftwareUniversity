4e695c8df557d81b9bd1fd7deb8cd1de0c983e66
Georgi <georgi.stoyanov777@gmail.com>
2016-11-18 10:59:17 +0200

    Create 01.Money.cs

---
+++ .ProgramingBasics/ProgrammingBasicsExam-17July2016/01.Money.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Money
+{
+    public static void Main()
+    {
+        int bitcoins = int.Parse(Console.ReadLine());
+        var yuans = double.Parse(Console.ReadLine());
+        var commision = double.Parse(Console.ReadLine()) / 100.00;
+
+        var bitcoinsInLeva = bitcoins * 1168.0;
+        var yuansToDollars = yuans * 0.15;
+        var dollarsToLeva = yuansToDollars * 1.76;
+
+        var levas = bitcoinsInLeva + dollarsToLeva;
+        var euros = levas / 1.95;
+        euros -= commision * euros;
+        Console.WriteLine(euros);
+    }
+}
