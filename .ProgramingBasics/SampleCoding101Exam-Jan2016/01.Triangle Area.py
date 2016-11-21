49f792c39709b3ef9ae5a9d7e3e53e877b6684b1
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2016-11-21 12:19:08 +0200

    Create 01.Triangle Area.py

---
+++ .ProgramingBasics/SampleCoding101Exam-Jan2016/01.Triangle Area.py
@@ -0,0 +1,13 @@
+x1 = int(input())
+y1 = int(input())
+
+x2 = int(input())
+y2 = int(input())
+
+x3 = int(input())
+y3 = int(input())
+
+baseLength = abs(x2 - x3)
+height = abs(y1 - y2)
+area = (baseLength * height) / 2.0
+print(area)
