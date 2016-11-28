fccc825e65d957993fd5fa12436f3b3e944b2bf1
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2016-11-28 12:04:21 +0200

    Update 03.Generating-01Vector.java

--- Algorithm/01.Recursion/03.Generating-01Vector.java
+++ Algorithm/01.Recursion/03.Generating-01Vector.java
@@ -1,29 +1,34 @@
 import java.util.Scanner;
 
 public class Main {
-   private static void printVector(int[] v) {
-       for(int i : v)
-           System.out.print(i + " ");
-       System.out.println();
-   }
+    private static int counter = 0;
 
-   public static void gen01(int[] vector, int index) {
-       if (index < 0)
-           printVector(vector);
+    private static void printVector(int[] v) {
+        for(int i : v)
+            System.out.print(i + " ");
+        System.out.println();
+    }
 
-       else {
-           for (int i = 0; i <= 1; i++) {
-               vector[index] = i;
-               gen01(vector, index - 1);
-           }
-       }
-   }
+    private static void gen01(int[] vector, int index) {
+        if (index == -1) {
+            printVector(vector);
+            return;
+        }
 
-   public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-       System.out.print("Enter size for a vector: ");
-       int size = Integer.parseInt(scanner.nextLine());
-       int[] vector = new int[size];
-       gen01(vector, size - 1);
-   }
+        counter++;
+        for (int i = 0; i <= 1; i++) {
+            vector[index] = i;
+            gen01(vector, index - 1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter size for a vector: ");
+        int size = Integer.parseInt(scanner.nextLine());
+        int[] vector = new int[size];
+        gen01(vector, size - 1);
+        System.out.println(counter);
+    }
 }
