fe684364f9ecdd6a21d24800fabf4e92d53eaeee
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2016-12-01 11:10:01 +0200

    Create 07.ExchangeVariableValues.java

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Exercises/07.ExchangeVariableValues.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ExchangeVariableValues {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Before:");
+        System.out.printf("a = %d%n", a);
+        System.out.printf("b = %d%n", b);
+
+        System.out.println("After:");
+        System.out.printf("a = %d%n", b);
+        System.out.printf("b = %d%n", a);
+   }
+}
