973c7cec6d083d2d33f937a2f2019b309c05c1f7
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2016-12-13 16:43:20 +0200

    Create 03.EnglishNameOfLastDigit.java

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/MethodsAndDebugging-Excercises/03.EnglishNameOfLastDigit.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long num = Long.parseLong(scanner.nextLine());
+        int lastDigit = (int)(Math.abs(num % 10));
+
+        switch (lastDigit) {
+            case 0:
+                System.out.println("zero");
+                break;
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            case 4:
+                System.out.println("four");
+                break;
+            case 5:
+                System.out.println("five");
+                break;
+            case 6:
+                System.out.println("six");
+                break;
+            case 7:
+                System.out.println("seven");
+                break;
+            case 8:
+                System.out.println("eight");
+                break;
+            case 9:
+                System.out.println("nine");
+                break;
+            default:
+                System.out.println();
+        }
+    }
+}
