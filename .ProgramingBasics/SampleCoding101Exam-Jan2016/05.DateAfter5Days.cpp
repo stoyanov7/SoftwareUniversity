3a819f154e4f7848cc09ed15de6e7ccac6605acc
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2016-12-14 18:27:46 +0200

    Update 05.DateAfter5Days.cpp

--- .ProgramingBasics/SampleCoding101Exam-Jan2016/05.DateAfter5Days.cpp
+++ .ProgramingBasics/SampleCoding101Exam-Jan2016/05.DateAfter5Days.cpp
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MergeSort {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int day = Integer.parseInt(scanner.nextLine());
