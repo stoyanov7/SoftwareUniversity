9c75f38f2dbfb30eec371d5a72dc55d1e7946ffe
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2016-12-14 18:27:28 +0200

    Update 05.DateAfter5Days.cpp

--- .ProgramingBasics/SampleCoding101Exam-Jan2016/05.DateAfter5Days.cpp
+++ .ProgramingBasics/SampleCoding101Exam-Jan2016/05.DateAfter5Days.cpp
@@ -1,65 +1,64 @@
-#include<iostream>
-using namespace std;
+import java.util.Scanner;
 
-int main() {
-	int day;
-	cin >> day;
+public class MergeSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int day = Integer.parseInt(scanner.nextLine());
+        int month = Integer.parseInt(scanner.nextLine());
 
-	int month;
-	cin >> month;
+        switch(month) {
+            case 2:
+                if (day + 5 > 28) {
+                    month++;
+                    day = day + 5 - 28;
+                }
+                else {
+                    day += 5;
+                }
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                if (day + 5 > 30) {
+                    month++;
+                    day = day + 5 - 30;
+                }
+                else {
+                    day += 5;
+                }
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+                if (day + 5 > 31) {
+                    month++;
+                    day = day + 5 - 31;
+                }
+                else {
+                    day += 5;
+                }
+                break;
+            case 12:
+                if (day + 5 > 31) {
+                    month = 1;
+                    day = day + 5 - 31;
+                }
+                else {
+                    day += 5;
+                }
+                break;
+            default:
+                break;
+        }
 
-	switch(month) {
-		case 2:
-			if (day + 5 > 28) {
-				month++;
-				day = day + 5 - 28;
-			}
-			else {
-				day += 5;
-			}
-			break;
-		case 4:
-		case 6:
-		case 9:
-		case 11:
-			if (day + 5 > 30) {
-				month++;
-				day = day + 5 - 30;
-			}
-			else {
-				day += 5;
-			}
-			break;
-		case 1:
-		case 3:
-		case 5:
-		case 7:
-		case 8:
-		case 10:
-			if (day + 5 > 31) {
-				month++;
-				day = day + 5 - 31;
-			}
-			else {
-				day += 5;
-			}
-			break;
-		case 12:
-			if (day + 5 > 31) {
-				month = 1;
-				day = day + 5 - 31;
-			}
-			else {
-				day += 5;
-			}
-			break;
-		default:
-			break;
-	}
-
-	cout << day << ".";
-	if (month < 10) {
-		cout << "0";
-	}
-  	cout << month;
+        System.out.print(day + ".");
+        if (month < 10) {
+            System.out.print("0");
+        }
+        System.out.print(month);
+    }
 }
