8e32dbf40c28302e741b488df62478a4214bf669
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2016-12-15 17:04:52 +0200

    Create 08.RefactorVolumeOfPyramid.py

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Lab/08.RefactorVolumeOfPyramid.py
@@ -0,0 +1,6 @@
+length = float(input("Length: "))
+width = float(input("Width: "))
+height = float(input("Height: "))
+
+volumе = (length * width * height) / 3
+print("Pyramid Volume: %.2f" % volumе)
