59d3ff3fbbaa31d2eaf85aaf179498f0e2c0a426
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2016-12-20 11:00:58 +0200

    Update switch case and using ternary operator

--- .ProgramingBasics/.ComplexConditionalStatements/08.TradeComissions.java
+++ .ProgramingBasics/.ComplexConditionalStatements/08.TradeComissions.java
@@ -1,55 +1,51 @@
+import java.text.MessageFormat;
 import java.util.Scanner;
 
-public class Main {
+public class TradeComissions {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String town = scanner.nextLine();
         double sales = Double.parseDouble(scanner.nextLine());
+        double comission = -1;
 
-        if (town.equals("Sofia")) {
-            if (sales >= 0 && sales <= 500) {
-                System.out.printf("%.2f", sales * 0.05);
-            }
-            else if (sales > 500 && sales <= 1000) {
-                System.out.printf("%.2f", sales * 0.07);
-            }
-            else if (sales > 1000 && sales <= 10000) {
-                System.out.printf("%.2f", sales * 0.08);
-            }
-            else if (sales > 10000) {
-                System.out.printf("%.2f", sales * 0.12);
-            }
+        switch (town) {
+            case "Sofia":
+                if (sales >= 0 && sales <= 500) {
+                    comission = sales * 0.05;
+                } else if (sales > 500 && sales <= 1000) {
+                    comission = sales * 0.07;
+                } else if (sales > 1000 && sales <= 10000) {
+                    comission = sales * 0.08;
+                } else if (sales > 10000) {
+                    comission = sales * 0.12;
+                }
+                break;
+            case "Varna":
+                if (sales >= 0 && sales <= 500) {
+                    comission = sales * 0.045;
+                } else if (sales > 500 && sales <= 1000) {
+                    comission = sales * 0.075;
+                } else if (sales > 1000 && sales <= 10000) {
+                    comission = sales * 0.10;
+                } else if (sales > 10000) {
+                    comission = sales * 0.13;
+                }
+                break;
+            case "Plovdiv":
+                if (sales >= 0 && sales <= 500) {
+                    comission = sales * 0.055;
+                } else if (sales > 500 && sales <= 1000) {
+                    comission = sales * 0.08;
+                } else if (sales > 1000 && sales <= 10000) {
+                    comission = sales * 0.12;
+                } else if (sales > 10000) {
+                    comission = sales * 0.145;
+                }
+                break;
+            default:
+                break;
         }
-        else if (town.equals("Varna")) {
-            if (sales >= 0 && sales <= 500) {
-                System.out.printf("%.2f", sales * 0.045);
-            }
-            else if (sales > 500 && sales <= 1000) {
-                System.out.printf("%.2f", sales * 0.075);
-            }
-            else if (sales > 1000 && sales <= 10000) {
-                System.out.printf("%.2f", sales * 0.10);
-            }
-            else if (sales > 10000) {
-                System.out.printf("%.2f", sales * 0.13);
-            }
-        }
-        else if (town.equals("Plovdiv")) {
-            if (sales >= 0 && sales <= 500) {
-                System.out.printf("%.2f", sales * 0.055);
-            }
-            else if (sales > 500 && sales <= 1000) {
-                System.out.printf("%.2f", sales * 0.08);
-            }
-            else if (sales > 1000 && sales <= 10000) {
-                System.out.printf("%.2f", sales * 0.12);
-            }
-            else if (sales > 10_000) {
-                System.out.printf("%.2f", sales * 0.0145);
-            }
-        }
-        else {
-            System.out.println("error");
-        }
+
+        System.out.println(comission >= 0 ? String.format("%.2f", comission) : "error");
     }
 }
