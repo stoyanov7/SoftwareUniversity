2f41906afcac4e635b8a9e206c0fd5f36237d938
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2016-12-31 14:42:51 +0200

    Create 09.RefactorSpecialNumbers.cs

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Lab/09.RefactorSpecialNumbers.cs
@@ -0,0 +1,22 @@
+using System;
+
+class RefactorSNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            int temporaryNumber = i;
+            int sum = 0;
+            while (temporaryNumber > 0)
+            {
+                sum += temporaryNumber % 10;
+                temporaryNumber /=  10;
+            }
+            bool special = (sum == 5) || (sum == 7) || (sum == 11);
+            Console.WriteLine($"{i} -> {special}");
+            sum = 0;
+        }
+    }
+}
