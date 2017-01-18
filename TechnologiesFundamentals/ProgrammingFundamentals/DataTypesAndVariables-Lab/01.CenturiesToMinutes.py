1f9ffb385e465d15d163223d1ea280079c18decf
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-18 19:46:03 +0200

    Delete 01.CenturiesToMinutes.py

--- TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Lab/01.CenturiesToMinutes.py
+++
@@ -1,8 +0,0 @@
-centuries = int(input())
-years = centuries * 100
-days = int(years * 365.2422)
-hours = days * 24
-minutes = hours * 60
-
-print("%d centuries = %d years = %d days = %d hours %d = minutes" %
-    (centuries, years, days, hours, minutes))
