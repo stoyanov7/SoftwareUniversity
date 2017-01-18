fc0064096f9b02c0944307e9a13196b284783c4d
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-18 20:07:31 +0200

    Update CircleAreaPrecision

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Lab/02.CircleAreaPrecision12/02.CircleAreaPrecision12/CircleAreaPrecision.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class CircleAreaPrecision
+{
+    static void Main(string[] args)
+    {
+        var radius = double.Parse(Console.ReadLine());
+        Console.WriteLine("{0:F12}", (Math.PI * Math.Pow(radius, 2)));
+    }
+}
