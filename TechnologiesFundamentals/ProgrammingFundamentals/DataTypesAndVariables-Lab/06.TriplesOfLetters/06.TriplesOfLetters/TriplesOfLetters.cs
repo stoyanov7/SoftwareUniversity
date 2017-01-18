aa1c693f37ef4ece0d117b5da13e223a8d1b290e
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-18 20:53:45 +0200

    Update TriplesOfLetters

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Lab/06.TriplesOfLetters/06.TriplesOfLetters/TriplesOfLetters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class TriplesOfLetters
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int firstLetter = 0; firstLetter < n; firstLetter++)
+        {
+            for (int secondLetter = 0; secondLetter < n; secondLetter++)
+            {
+                for (int thirdLetter = 0; thirdLetter < n; thirdLetter++)
+                {
+                    Console.WriteLine("{0}{1}{2}", 
+                        (char)('a' + firstLetter), (char)('a' + secondLetter), (char)('a' + thirdLetter));
+                }
+            }
+        }
+    }
+}
