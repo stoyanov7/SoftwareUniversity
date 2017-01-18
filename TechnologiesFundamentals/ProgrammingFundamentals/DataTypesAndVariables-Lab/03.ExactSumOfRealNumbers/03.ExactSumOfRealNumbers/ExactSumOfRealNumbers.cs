3c05e02de54ef02a1035656b3b8c3c003d62686d
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-18 20:52:33 +0200

    Update ExactSumOfRealNumbers

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Lab/03.ExactSumOfRealNumbers/03.ExactSumOfRealNumbers/ExactSumOfRealNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class ExactSumOfRealNumbers
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        decimal sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            decimal value = decimal.Parse(Console.ReadLine());
+            sum += value;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
