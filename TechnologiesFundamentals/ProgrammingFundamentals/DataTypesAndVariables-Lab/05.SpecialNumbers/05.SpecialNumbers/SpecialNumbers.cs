a908315b95d2035f82eccc84ac6b39969434b1dd
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-18 20:51:49 +0200

    Update SpecialNumbers

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Lab/05.SpecialNumbers/05.SpecialNumbers/SpecialNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class SpecialNumbers
+{
+    public static void Main(string[] args)
+    {
+        var n = int.Parse(Console.ReadLine());
+
+        for (var i = 1; i <= n; i++)
+        {
+            var sumOfDigits = 0;
+            var number = i;
+
+            while (number > 0)
+            {
+                sumOfDigits += number % 10;
+                number = number / 10;
+            }
+
+            bool isSpecial = (sumOfDigits == 5) || (sumOfDigits == 7) || (sumOfDigits == 11);
+            Console.WriteLine($"{i} -> {isSpecial}");
+        }
+    }
+}
