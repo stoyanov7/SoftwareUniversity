7320a170d95c483874b3b9a3a6305d9d266125ae
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-18 20:54:31 +0200

    Update RefactorVolumeOfPyramid

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Lab/08.RefactorVolumeOfPyramid/08.RefactorVolumeOfPyramid/RefactorVolumeOfPyramid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class RefactorVolumeOfPyramid
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Length: ");
+        var length = float.Parse(Console.ReadLine());
+
+        Console.Write("Width: ");
+        var width = float.Parse(Console.ReadLine());
+
+        Console.Write("Height: ");
+        var height = float.Parse(Console.ReadLine());
+
+        var volumе = (length * width * height) / 3;
+        Console.Write("Pyramid Volume: {0:F2}", volumе);
+    }
+}
