3de7c8ccb9545f3c91f93ee63f3baa41b9dacce5
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-18 20:53:04 +0200

    Update Elevator

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Lab/04.Elevator/04.Elevator/Elevator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Elevator
+{
+    public static void Main(string[] args)
+    {
+        var numberOfPeople = int.Parse(Console.ReadLine());
+        var capacity = int.Parse(Console.ReadLine());
+
+        var couses = (int)Math.Ceiling((double)numberOfPeople / capacity);
+        Console.WriteLine(couses); 
+    }
+}
