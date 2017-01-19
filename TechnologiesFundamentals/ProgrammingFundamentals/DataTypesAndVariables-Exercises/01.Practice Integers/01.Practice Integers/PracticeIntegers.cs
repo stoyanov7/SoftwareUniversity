09978e189293d3d1e58ef3f581979d0fdeec7b44
stoyanov7 <georgi.stoyanov777@gmail.com>
2017-01-19 19:10:25 +0200

    Create PracticeIntegers

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Exercises/01.Practice Integers/01.Practice Integers/PracticeIntegers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class PracticeIntegerNumbers
+{
+    public static void Main()
+    {
+        sbyte firstNumber = sbyte.Parse(Console.ReadLine());
+        byte secondNumber = byte.Parse(Console.ReadLine());
+        short thirdNumber = short.Parse(Console.ReadLine());
+        ushort forthNumber = ushort.Parse(Console.ReadLine());
+        uint fifthNumber = uint.Parse(Console.ReadLine());
+        int sixtNumber = int.Parse(Console.ReadLine());
+        long seventhNumber = long.Parse(Console.ReadLine());
+
+        Console.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}",
+            firstNumber, secondNumber, thirdNumber, forthNumber, fifthNumber, sixtNumber, seventhNumber);
+    }
+}
---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Exercises/01.Practice Integers/01.Practice Integers/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("01.Practice Integers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("01.Practice Integers")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5807ba96-eb1a-4bc0-9490-338f37f71000")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
