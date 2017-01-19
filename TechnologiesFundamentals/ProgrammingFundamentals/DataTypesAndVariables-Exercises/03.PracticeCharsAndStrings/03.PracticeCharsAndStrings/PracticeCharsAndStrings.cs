26823dbc2c7182a06b44a3aa53c51d889283e03e
stoyanov7 <georgi.stoyanov777@gmail.com>
2017-01-19 19:13:24 +0200

    Create PracticeCharsAndStrings

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Exercises/03.PracticeCharsAndStrings/03.PracticeCharsAndStrings/PracticeCharsAndStrings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class PracticeCharsAndStrings
+{
+    public static void Main(string[] args)
+    {
+        string firstInput = Console.ReadLine();
+        char secondInput = char.Parse(Console.ReadLine());
+        char thirdInput = char.Parse(Console.ReadLine());
+        char fourthInput = char.Parse(Console.ReadLine());
+        string fifthInput = Console.ReadLine();
+
+        Console.WriteLine($"{firstInput}\n{secondInput}\n{thirdInput}\n{fourthInput}\n{fifthInput}");
+    }
+}
