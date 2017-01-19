059ab69cfb00bb7972afa18e7d2b3f797683f95b
stoyanov7 <georgi.stoyanov777@gmail.com>
2017-01-19 19:11:53 +0200

    Create PracticeFloatingPoints

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Exercises/02.PracticeFloatingPoints/02.PracticeFloatingPoints/PracticeFloatingPoints.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class PracticeFloatingPoints
+{
+    public static void Main(string[] args)
+    {
+        decimal firstNumber = decimal.Parse(Console.ReadLine());
+        double secondNumber = double.Parse(Console.ReadLine());
+        decimal thirdNumber = decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{firstNumber}\n{secondNumber}\n{thirdNumber}");
+    }
+}
