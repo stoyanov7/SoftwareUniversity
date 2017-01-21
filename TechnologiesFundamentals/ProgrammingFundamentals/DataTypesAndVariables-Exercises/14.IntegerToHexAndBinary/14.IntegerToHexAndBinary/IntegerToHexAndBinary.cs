db4ad18d923bc546b2d0afabc607fcfee893fd7d
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-21 10:16:53 +0200

    Create IntegerToHexAndBinary

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Exercises/14.IntegerToHexAndBinary/14.IntegerToHexAndBinary/IntegerToHexAndBinary.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class IntegerToHexAndBinary
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(Convert.ToString(n, 16).ToUpper());
+        Console.WriteLine(Convert.ToString(n, 2));
+    }
+}
