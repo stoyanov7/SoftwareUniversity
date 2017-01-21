0d89c0cb426c8e03c0acd771d9272bfc41594b73
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-21 10:00:53 +0200

    Create BooleanVariable

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Exercises/05.BooleanVariable/05.BooleanVariable/BooleanVariable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class BooleanVariable
+{
+    public static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        bool result = Convert.ToBoolean(input);
+
+        Console.WriteLine(result ? "Yes" : "No");
+    }
+}
