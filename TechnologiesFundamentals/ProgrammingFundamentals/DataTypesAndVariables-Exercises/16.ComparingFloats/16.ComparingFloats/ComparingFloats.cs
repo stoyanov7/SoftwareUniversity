08ca95211b7eae7b093e1e322e66fac626449f26
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-21 10:17:34 +0200

    Create ComparingFloats

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Exercises/16.ComparingFloats/16.ComparingFloats/ComparingFloats.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class ComparingFloats
+{
+    public static void Main(string[] args)
+    {
+        double firstNumber = double.Parse(Console.ReadLine());
+        double secondNumber = double.Parse(Console.ReadLine());
+        double eps = 0.000001;
+
+        Console.WriteLine(Math.Abs(firstNumber - secondNumber) < eps ? "True" : "False");
+    }
+}
