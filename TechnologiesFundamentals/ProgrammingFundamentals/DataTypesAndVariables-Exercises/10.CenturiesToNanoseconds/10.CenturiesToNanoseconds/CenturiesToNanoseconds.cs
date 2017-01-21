91f9c2d4898429bc1f558236a6be7aa45e4c3460
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-21 10:03:45 +0200

    Create CenturiesToNanoseconds

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Exercises/10.CenturiesToNanoseconds/10.CenturiesToNanoseconds/CenturiesToNanoseconds.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class CenturiesToNanoseconds
+{
+    public static void Main(string[] args)
+    {
+        var centuries = int.Parse(Console.ReadLine());
+        var years = centuries * 100;
+        var days = (int)(years * 365.2422);
+        var hours = days * 24;
+        var minutes = hours * 60;
+        var seconds = minutes * 60; 
+        var milliseconds = seconds * 1000;
+        var microseconds = milliseconds * 1000;
+        var nanoseconds = microseconds * 1000;
+
+        Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes = {seconds} seconds = {milliseconds} milliseconds = {microseconds} microseconds = {nanoseconds} nanoseconds");
+    }
+}
