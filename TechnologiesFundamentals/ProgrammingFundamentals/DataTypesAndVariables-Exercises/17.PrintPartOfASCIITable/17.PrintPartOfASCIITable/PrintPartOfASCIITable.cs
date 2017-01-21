d393ccb68742aa30a7284a3457ff8129785656cd
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-21 10:17:58 +0200

    Create PrintPartOfASCIITable

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Exercises/17.PrintPartOfASCIITable/17.PrintPartOfASCIITable/PrintPartOfASCIITable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class PrintPartOfASCIITable
+{
+    public static void Main(string[] args)
+    {
+        int start = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+
+        for (int i = start; i <= end; i++)
+        {
+            Console.Write((char)i + " ");
+        }
+    }
+}
