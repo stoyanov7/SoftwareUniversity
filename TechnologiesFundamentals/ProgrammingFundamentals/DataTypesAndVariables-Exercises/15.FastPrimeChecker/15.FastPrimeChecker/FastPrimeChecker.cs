8e992052472a3f001a212184a31ccab190fe435c
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-21 10:17:13 +0200

    Create FastPrimeChecker

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Exercises/15.FastPrimeChecker/15.FastPrimeChecker/FastPrimeChecker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class FastPrimeChecker
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 2; i <= n; i++)
+        {
+            bool isPrime = true;
+            for (int divisor = 2; divisor <= Math.Sqrt(i); divisor++)
+            {
+                if (i % divisor == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            Console.WriteLine(isPrime ? ($"{i} -> True") : ($"{i} -> False"));
+        }
+    }
+}
