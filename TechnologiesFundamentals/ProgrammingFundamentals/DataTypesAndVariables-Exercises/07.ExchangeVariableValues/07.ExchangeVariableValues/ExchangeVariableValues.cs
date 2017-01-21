c31931b5165aef29b54364e5c054bae8904d21cc
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-21 10:02:19 +0200

    Create ExchangeVariableValues

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Exercises/07.ExchangeVariableValues/07.ExchangeVariableValues/ExchangeVariableValues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class ExchangeVariableValues
+{
+    static void Main(string[] args)
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Before:\na = {firstNumber}\nb = {secondNumber}");
+        Console.WriteLine($"After:\na = {secondNumber}\nb = {firstNumber}");
+    }
+}
