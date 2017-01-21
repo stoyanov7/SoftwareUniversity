401493222b9ebfafed3940967d7c0cac6171012d
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-21 10:00:10 +0200

    Create VariableInHexFormat

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Exercises/04.VariableInHexFormat/04.VariableInHexFormat/VariableInHexFormat.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class VariableInHexFormat
+{
+    public static void Main(string[] args)
+    {
+        string variable = Console.ReadLine();
+        Console.WriteLine(Convert.ToInt32(variable, 16));
+    }
+}
