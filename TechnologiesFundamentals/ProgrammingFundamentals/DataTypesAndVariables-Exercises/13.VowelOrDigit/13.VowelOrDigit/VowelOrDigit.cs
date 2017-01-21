046f79d66d8c7d5d91a5579d0e19b5cd4c31cd68
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-21 10:05:18 +0200

    Create VowelOrDigit

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Exercises/13.VowelOrDigit/13.VowelOrDigit/VowelOrDigit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class VowelOrDigit
+{
+    public static void Main(string[] args)
+    {
+        char symbol = char.Parse(Console.ReadLine());
+        bool isVowel = (symbol == 'a') || (symbol == 'e') || (symbol == 'i') ||
+            (symbol == 'o') || (symbol == 'u');
+
+        bool isDigit = (symbol >= '0') && (symbol <= '9');
+
+        if (isVowel)
+        {
+            Console.WriteLine("vowel");
+        }
+        else if (isDigit)
+        {
+            Console.WriteLine("digit");
+        }
+        else
+        {
+            Console.WriteLine("other");
+        }
+    }
+}
