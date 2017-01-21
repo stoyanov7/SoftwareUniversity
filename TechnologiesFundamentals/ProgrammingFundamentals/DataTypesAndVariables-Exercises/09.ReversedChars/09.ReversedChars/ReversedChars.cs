e1e577fed8683e1a7986b2ba7196daf86bc3c19d
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-21 10:03:21 +0200

    Create ReversedChars

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Exercises/09.ReversedChars/09.ReversedChars/ReversedChars.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class ReversedChars
+{
+    public static void Main(string[] args)
+    {
+        string firstInput = Console.ReadLine();
+        string seconsInput = Console.ReadLine();
+        string thirdInput = Console.ReadLine();
+
+        Console.WriteLine($"{thirdInput}{seconsInput}{firstInput}");
+    }
+}
