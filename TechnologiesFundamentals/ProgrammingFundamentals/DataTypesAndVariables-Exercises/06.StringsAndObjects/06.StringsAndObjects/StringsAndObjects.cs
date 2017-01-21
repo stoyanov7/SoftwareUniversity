4314f0bec6ac29bbc5f5ffb253b1277e6fb8e975
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-21 10:01:18 +0200

    Create StringsAndObjects

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Exercises/06.StringsAndObjects/06.StringsAndObjects/StringsAndObjects.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class StringsAndObjects
+{
+    public static void Main(string[] args)
+    {
+        string firstInput = Console.ReadLine();
+        string secondInput = Console.ReadLine();
+        string text = firstInput + " " + secondInput;
+
+        Console.WriteLine(text);
+    }
+}
