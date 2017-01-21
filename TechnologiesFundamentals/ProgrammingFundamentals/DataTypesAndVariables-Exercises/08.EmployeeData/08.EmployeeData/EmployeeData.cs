b1f16fbf9ff9768fc18686b13c930b525d996bec
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-21 10:02:47 +0200

    Create EmployeeData

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Exercises/08.EmployeeData/08.EmployeeData/EmployeeData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class EmployeeData
+{
+    public static void Main(string[] args)
+    {
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+        string gender = Console.ReadLine();
+        long personalID = long.Parse(Console.ReadLine());
+        long uniqueNumber = long.Parse(Console.ReadLine());
+
+        Console.WriteLine($"First name: {firstName}");
+        Console.WriteLine($"Last name: {lastName}");
+        Console.WriteLine($"Age: {age}");
+        Console.WriteLine($"Gender: {gender}");
+        Console.WriteLine($"Personal ID: {personalID}");
+        Console.WriteLine($"Unique Employee number: {uniqueNumber}");
+    }
+}
