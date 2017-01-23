cebf6457349c5c307f7982695d1e125cea8382be
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-23 22:57:57 +0200

    Create SignOfIntegerNumber

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/MethodsAndDebugging-Lab/02.SignOfIntegerNumber/02.SignOfIntegerNumber/SignOfIntegerNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class SignOfIntegerNumber
+{
+    public static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+        PrintSign(number);
+    }
+
+    private static void PrintSign(int num)
+    {
+        if (num > 0)
+        {
+            Console.WriteLine($"The number {num} is positive.");
+        }
+
+        else if (num < 0)
+        {
+            Console.WriteLine($"The number {num} is negative.");
+        }
+        else if (num == 0)
+        {
+            Console.WriteLine($"The number {num} is zero.");
+        }
+    }
+}
