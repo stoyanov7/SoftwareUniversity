3e5c4f9b6477f86c233296c137b8cc4188917ef0
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-23 22:58:52 +0200

    Create DrawAFilledSquare

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/MethodsAndDebugging-Lab/04. DrawAFilledSquare/04. DrawAFilledSquare/DrawAFilledSquare.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class DrawAFilledSquare
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        PrintHeaderRow(n);
+        PrintMiddleRow(n);
+        PrintHeaderRow(n);
+    }
+
+    private static void PrintHeaderRow(int n)
+    {
+        Console.WriteLine(new string('-', 2 * n));
+    }
+
+    private static void PrintMiddleRow(int n)
+    {
+        for (int i = 0; i < n - 2; i++)
+        {
+            Console.Write("-");
+            for (int j = 1; j < n; j++)
+            {
+                Console.Write("\\/");
+            }
+            Console.WriteLine("-");
+        }
+    }
+}
