a9d383a8da4809b869c079a3db940bd661f0174c
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-23 22:58:20 +0200

    Create PrintingTriangle

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/MethodsAndDebugging-Lab/03.PrintingTriangle/03.PrintingTriangle/PrintingTriangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class PrintingTriangle
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            PrintLine(1, i);
+        }
+
+        PrintLine(1, n);
+
+        for (int j = n - 1; j >= 0; j--)
+        {
+            PrintLine(1, j);
+        }
+    }
+
+    private static void PrintLine(int start, int end)
+    {
+        for (int i = start; i <= end; i++)
+        {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+    }
+}
