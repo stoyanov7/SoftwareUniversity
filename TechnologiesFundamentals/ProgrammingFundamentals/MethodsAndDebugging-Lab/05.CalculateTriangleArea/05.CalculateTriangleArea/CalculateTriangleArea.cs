8256c4c8f7cfe27af6dc9b420c33da07392bc4ed
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-23 22:59:16 +0200

    Create CalculateTriangleArea

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/MethodsAndDebugging-Lab/05.CalculateTriangleArea/05.CalculateTriangleArea/CalculateTriangleArea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class CalculateTriangleArea
+{
+    public static void Main(string[] args)
+    {
+        double width = double.Parse(Console.ReadLine());
+        double height = double.Parse(Console.ReadLine());
+        double area = GetTriangleArea(width, height);
+        Console.WriteLine(area);
+    }
+
+    private static double GetTriangleArea(double width, double height)
+    {
+        return width * height / 2;
+    }
+}
