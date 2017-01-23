492e77db56d5ecca7e84731b5c7a79483e741cb3
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-23 22:59:42 +0200

    Create MathPower

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/MethodsAndDebugging-Lab/06.MathPower/06.MathPower/MathPower.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class MathPower
+{
+    public static void Main(string[] args)
+    {
+        double number = double.Parse(Console.ReadLine());
+        double power = double.Parse(Console.ReadLine());
+        Console.WriteLine($"{RaiseToPower(number, power)}");
+    }
+
+    private static double RaiseToPower(double number, double power)
+    {
+        double result = 1;
+
+        for (int i = 0; i < power; i++)
+        {
+            result *= number;
+        }
+
+        return result;
+    }
+}
