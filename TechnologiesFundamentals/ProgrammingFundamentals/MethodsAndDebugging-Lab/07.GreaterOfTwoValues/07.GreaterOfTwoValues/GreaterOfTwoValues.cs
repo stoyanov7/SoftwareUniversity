caea4bddf7ffd78250e067c8a158fac846d56ec2
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-23 23:00:08 +0200

    Create GreaterOfTwoValues

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/MethodsAndDebugging-Lab/07.GreaterOfTwoValues/07.GreaterOfTwoValues/GreaterOfTwoValues.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class GreaterOfTwoValues
+{
+    public static void Main(string[] args)
+    {
+        string type = Console.ReadLine();
+
+        switch (type)
+        {
+            case "int":
+                int firstInt = int.Parse(Console.ReadLine());
+                int secondInt = int.Parse(Console.ReadLine());
+                Console.WriteLine(GetMax(firstInt, secondInt));
+                break;
+
+            case "char":
+                char firstChar = char.Parse(Console.ReadLine());
+                char secondChar = char.Parse(Console.ReadLine());
+                Console.WriteLine(GetMax(firstChar, secondChar));
+                break;
+
+            case "string":
+                string firstString = Console.ReadLine();
+                string secondString = Console.ReadLine();
+                Console.WriteLine(GetMax(firstString, secondString));
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private static int GetMax(int first, int second)
+    {
+        return first >= second ? first : second;
+    }
+
+    private static char GetMax(char first, char second)
+    {
+        return (char)GetMax((int)first, (int)second);
+    }
+
+    private static string GetMax(string first, string second)
+    {
+        return first.CompareTo(second) >= 0 ? first : second;
+    }
+}
