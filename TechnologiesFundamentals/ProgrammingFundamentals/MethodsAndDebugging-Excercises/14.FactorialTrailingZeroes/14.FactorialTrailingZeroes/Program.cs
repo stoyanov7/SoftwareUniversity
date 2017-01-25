9733a3bf85b9febf7948c5a72bf4893e02deb095
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-25 10:21:23 +0200

    Create FactorialTrailingZeroes

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/MethodsAndDebugging-Excercises/14.FactorialTrailingZeroes/14.FactorialTrailingZeroes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Numerics;
+
+public class FactorialTrailingZeroes
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        GetTrailingZeroes(Factorial(n));
+    }
+
+    private static BigInteger Factorial(int n)
+    {
+        BigInteger factorial = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            factorial *= i;
+        }
+
+        return factorial;
+    }
+
+    private static void GetTrailingZeroes(BigInteger number)
+    {
+        BigInteger zeroCount = 0;
+        while (number % 10 == 0)
+        {
+            number /= 10;
+            zeroCount++;
+        }
+
+        Console.WriteLine(zeroCount);
+    }
+}
