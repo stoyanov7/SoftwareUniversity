64b263013011e231787aa25b0b87773c467bbebb
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-25 10:15:40 +0200

    Create MaxMethod

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/MethodsAndDebugging-Excercises/02.MaxMethod/02.MaxMethod/MaxMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class MaxMethod
+{
+    public static void Main(string[] args)
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+        int thirdNumber = int.Parse(Console.ReadLine());
+
+        int firstAndSecond = GetMax(firstNumber, secondNumber);
+        int biggest = GetMax(firstAndSecond, thirdNumber);
+
+        Console.WriteLine(biggest);
+    }
+
+    private static int GetMax(int a, int b)
+    {
+        return Math.Max(a, b);
+    }
+}
