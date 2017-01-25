abdaa7d85726688050daed64362bf64ead75e663
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-25 10:17:08 +0200

    Create FibonacciNumbers

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/MethodsAndDebugging-Excercises/05.FibonacciNumbers/05.FibonacciNumbers/FibonacciNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class FibonacciNumbers
+{
+    public static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine(Fibonacci(number));
+    }
+
+    private static long Fibonacci(int number)
+    {
+        if (number == 0 || number == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return Fibonacci(number - 1) + Fibonacci(number - 2);
+        }
+    }
+}
