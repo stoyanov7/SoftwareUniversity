b27d4e0b31bc6afe92ab5ce26d4e1f31debc408d
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-25 10:20:16 +0200

    Create MasterNumber

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/MethodsAndDebugging-Excercises/12.MasterNumber/12.MasterNumber/MasterNumber.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+public class MasterNumber
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i <= n; i++)
+        {
+            if (IsPalindrome(i.ToString()) && SumDiv(i))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    private static bool IsPalindrome(string n)
+    {
+        for (int i = 0; i < n.Length / 2; i++)
+        {
+            if (n[i] != n[n.Length - 1 - i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static bool SumDiv(int n)
+    {
+        bool evenDigit = false;
+        bool divisible = false;
+        int sum = 0;
+
+        while (n != 0)
+        {
+            int lastDigit = n % 10;
+
+            if (lastDigit % 2 == 0)
+            {
+                evenDigit = true;
+            }
+
+            sum += lastDigit;
+            n /= 10;
+        }
+
+        if (sum % 7 == 0)
+        {
+            divisible = true;
+        }
+
+        if (evenDigit && divisible)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
