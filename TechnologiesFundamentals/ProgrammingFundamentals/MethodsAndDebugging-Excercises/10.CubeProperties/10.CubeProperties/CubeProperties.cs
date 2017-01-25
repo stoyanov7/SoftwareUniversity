7481698916b6a207c900782ce3de9eccf6a14e6d
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-25 10:19:13 +0200

    Create CubeProperties

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/MethodsAndDebugging-Excercises/10.CubeProperties/10.CubeProperties/CubeProperties.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class CubeProperties
+{
+    public static void Main(string[] args)
+    {
+        double cubeSide = double.Parse(Console.ReadLine());
+        string parameter = Console.ReadLine();
+        CalculateCubeProperties(cubeSide, parameter);
+    }
+
+    private static void CalculateCubeProperties(double cubeSide, string parameter)
+    {
+        if (parameter == "face")
+        {
+            double face = Math.Sqrt(Math.Pow(cubeSide, 2) * 2);
+            Console.WriteLine("{0:F2}", face);
+        }
+        else if (parameter == "space")
+        {
+            double space = Math.Sqrt(Math.Pow(cubeSide, 2) * 3);
+            Console.WriteLine("{0:F2}", space);
+        }
+        else if (parameter == "volume")
+        {
+            double volume = Math.Pow(cubeSide, 3);
+            Console.WriteLine("{0:F2}", volume);
+        }
+        else if (parameter == "area")
+        {
+            double area = Math.Pow(cubeSide, 2) * 6;
+            Console.WriteLine("{0:F2}", area);
+        }
+    }
+}
