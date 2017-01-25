4e1b6519423ee523029f25a66e59fb2c13cdc24c
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-25 10:16:33 +0200

    Create NumbersInReversedOrder

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/MethodsAndDebugging-Excercises/04.NumbersInReversedOrder/04.NumbersInReversedOrder/NumbersInReversedOrder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+public class NumbersInReversedOrder
+{
+    public static void Main(string[] args)
+    {
+        string number = Console.ReadLine();
+        ReverseNumber(number);
+    }
+
+    private static void ReverseNumber(string number)
+    {
+        for (int i = number.Length - 1; i >= 0; i--)
+        {
+            Console.Write(number[i]);
+        }
+    }
+}
