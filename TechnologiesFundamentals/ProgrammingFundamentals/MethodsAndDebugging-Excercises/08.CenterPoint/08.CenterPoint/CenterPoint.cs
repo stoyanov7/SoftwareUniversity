aa969cde1e55f7b6a915f8fde34d8d35676a8965
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-25 10:18:27 +0200

    Create CenterPoint

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/MethodsAndDebugging-Excercises/08.CenterPoint/08.CenterPoint/CenterPoint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class CenterPoint
+{
+    public static void Main(string[] args)
+    {
+        double x1 = double.Parse(Console.ReadLine());
+        double y1 = double.Parse(Console.ReadLine());
+        double x2 = double.Parse(Console.ReadLine());
+        double y2 = double.Parse(Console.ReadLine());
+
+        double distanceToCenterA = getDistanceBetweenTwoPoints(x1, y1);
+        double distanceToCenterB = getDistanceBetweenTwoPoints(x2, y2);
+
+        Console.WriteLine(distanceToCenterA < distanceToCenterB ? $"({x1}, {y1})" : $"({x2}, {y2})");
+    }
+
+    private static double getDistanceBetweenTwoPoints(double x, double y)
+    {
+        return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+    }
+}
