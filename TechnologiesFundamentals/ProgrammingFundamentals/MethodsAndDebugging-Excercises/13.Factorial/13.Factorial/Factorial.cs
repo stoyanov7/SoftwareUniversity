58ac46e029a3bb1ee2df7c161a12abd501504fac
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-25 10:20:43 +0200

    Create Factorial

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/MethodsAndDebugging-Excercises/13.Factorial/13.Factorial/Factorial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+public class Factorial
+{
+    public static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine(Factorials(number));
+    }
+
+    private static BigInteger Factorials(int number)
+    {
+        BigInteger factorial = 1;
+        for (int i = 2; i <= number; i++)
+        {
+            factorial *= i;
+        }
+
+        return factorial;
+    }
+}
