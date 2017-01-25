a661377d48511e2a2637de41b76ebcce0ad01da6
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-25 10:17:39 +0200

    Create PrimeChecker

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/MethodsAndDebugging-Excercises/06.PrimeChecker/06.PrimeChecker/PrimeChecker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class PrimeChecker
+{
+    public static void Main(string[] args)
+    {
+        long number = long.Parse(Console.ReadLine());
+        Console.WriteLine(IsPrime(number));
+    }
+
+    private static bool IsPrime(long number)
+    {
+        if (number < 2)
+        {
+            return false;
+        }
+        else
+        {
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
