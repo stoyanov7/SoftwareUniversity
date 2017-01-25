3dcf15464cac893bbf9793f7c164cc309a4e4fff
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-25 10:22:36 +0200

    Delete 05.FibonacciNumbers.py

--- TechnologiesFundamentals/ProgrammingFundamentals/MethodsAndDebugging-Excercises/05.FibonacciNumbers.py
+++
@@ -1,9 +0,0 @@
-def fibonacci(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
-
-if __name__ == '__main__':
-    number = int(input())
-    print(fibonacci(number))
