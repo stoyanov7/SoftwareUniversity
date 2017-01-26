fb2728d12c3e41d1630d1064aa5c6b323b7dfadc
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-26 10:25:06 +0200

    Create CondenseArrayToNumber

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ArraysAndLists-Lab/08.CondenseArrayToNumber/08.CondenseArrayToNumber/CondenseArrayToNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+public class CondenseArrayToNumber
+{
+    public static void Main(string[] args)
+    {
+        int[] numberArray = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+
+        while (numberArray.Length > 1)
+        {
+            int[] condensed = new int[numberArray.Length - 1];
+
+            for (int j = 0; j < numberArray.Length - 1; j++)
+            {
+                condensed[j] = numberArray[j] + numberArray[j + 1];
+            }
+            numberArray = condensed;
+        }
+
+        numberArray.ToList().ForEach(e => Console.WriteLine(e));
+    }
+}
