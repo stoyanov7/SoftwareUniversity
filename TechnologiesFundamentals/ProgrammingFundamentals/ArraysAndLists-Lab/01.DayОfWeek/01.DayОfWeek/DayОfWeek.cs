71a47a4717b920b2257e2507cdb8eec4602293db
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-26 09:54:12 +0200

    Create DayОfWeek

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ArraysAndLists-Lab/01.DayОfWeek/01.DayОfWeek/DayОfWeek.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class DayOfWeekend
+{
+    public static void Main(string[] args)
+    {
+        string[] daysOfWeek = 
+        {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"
+        };
+
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(n > 0 && n < 8 ? daysOfWeek[n - 1] : "Invalid day");
+    }
+}
