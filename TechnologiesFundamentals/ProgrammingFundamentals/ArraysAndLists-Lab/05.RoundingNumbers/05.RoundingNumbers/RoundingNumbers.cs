499137b0e183e97e6597d3d1514a3f4da3d71208
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-26 09:57:06 +0200

    Create RoundingNumbers

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ArraysAndLists-Lab/05.RoundingNumbers/05.RoundingNumbers/RoundingNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+public class RoundingNumbers
+{
+    public static void Main(string[] args)
+    {
+        double[] numbers = Console.ReadLine()
+            .Split(' ')
+            .Select(double.Parse).
+            ToArray();
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write($"{numbers[i]} => ");
+            numbers[i] = Math.Round(numbers[i], MidpointRounding.AwayFromZero);
+            Console.WriteLine(numbers[i]);
+        }
+    }
+}
