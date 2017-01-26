84d5b15c5deb6014e01a78aa4acf78d35906f5a6
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-26 09:55:11 +0200

    Create ReverseArrayOfIntegers

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ArraysAndLists-Lab/02.ReverseArrayOfIntegers/02.ReverseArrayOfIntegers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class ReverseArrayOfIntegers
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        var arr = new int[n];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            Console.WriteLine(arr[i]);
+        }
+    }
+}
