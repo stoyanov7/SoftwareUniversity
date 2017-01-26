fbb053a66226b41c212aaba706a2b2ccf430867c
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-26 10:24:40 +0200

    Create SumArrays

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ArraysAndLists-Lab/07.SumArrays/07.SumArrays/SumArrays.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+public class SumArrays
+{
+    public static void Main(string[] args)
+    {
+        int[] arrayOne = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse).
+            ToArray();
+
+        int[] arrayTwo = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse).
+            ToArray();
+
+        int[] sumArray = new int[Math.Max(arrayOne.Length, arrayTwo.Length)];
+
+        for (int i = 0; i < sumArray.Length; i++)
+        {
+            sumArray[i] = arrayOne[i % arrayOne.Length] + arrayTwo[i % arrayTwo.Length];
+        }
+
+        sumArray.ToList().ForEach(e => Console.WriteLine(e));
+    }
+}
