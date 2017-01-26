d651b8d15cedec68cb54e8ff37efc61b8bc65260
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-26 10:26:14 +0200

    Create ExtractMiddleElements

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ArraysAndLists-Lab/09.ExtractMiddleElements/09.ExtractMiddleElements/ExtractMiddleElements.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+public class ExtractMiddleElements
+{
+    public static void Main(string[] args)
+    {
+        int[] array = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+
+        if (array.Length == 1)
+        {
+            extractOne(array);
+        }
+        else if (array.Length % 2 == 0)
+        {
+            extractEven(array);
+        }
+        else
+        {
+            extractOdd(array);
+        }
+    }
+
+    private static void extractOne(int[] arr)
+    {
+        Console.WriteLine(arr[0]);
+    }
+
+    private static void extractEven(int[] arr)
+    {
+        Console.WriteLine(($"{arr[arr.Length / 2 - 1]}, {arr[arr.Length / 2]}"));
+    }
+
+    private static void extractOdd(int[] arr)
+    {
+        Console.WriteLine($"{arr[arr.Length / 2 - 1]}, {arr[arr.Length / 2]}, {arr[arr.Length / 2 + 1]}");
+    }
+}
