134b5e93159d752596b33b54bd4931d9f76fb594
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-26 10:24:14 +0200

    Create ReverseArrayOfStrings

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ArraysAndLists-Lab/06.ReverseArrayOfStrings/06.ReverseArrayOfStrings/ReverseArrayOfStrings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class ReverseArrayOfStrings
+{
+    public static void Main(string[] args)
+    {
+        string[] stringArray = Console.ReadLine().Split(' ');
+
+        for (int i = stringArray.Length - 1; i >= 0; i--)
+        {
+            Console.WriteLine(stringArray[i] + " ");
+        }
+    }
+}
