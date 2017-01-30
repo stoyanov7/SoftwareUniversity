c3b472a1fbf7de260f3061f8505f2fb0796bd80e
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-30 23:01:00 +0200

    Create TrippleSum

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ArraysAndLists-Excercises/04.TrippleSum/04.TrippleSum/TrippleSum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+public class TrippleSum
+{
+    public static void Main(string[] args)
+    {
+        int[] array = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse).
+            ToArray();
+        int counter = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                int sum = array[i] + array[j];
+
+                if (array.Contains(sum))
+                {
+                    Console.WriteLine($"{array[i]} + {array[j]} == {sum}");
+                    counter++;
+                }
+            }
+        }
+
+        if (counter == 0)
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
