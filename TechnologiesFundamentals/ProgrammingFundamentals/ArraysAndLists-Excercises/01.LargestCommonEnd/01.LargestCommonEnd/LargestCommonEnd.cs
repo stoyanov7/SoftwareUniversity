03c47aa0f1c43721fac6ebdbadb6b3b4213e657e
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-30 22:52:49 +0200

    Create LargestCommonEnd

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ArraysAndLists-Excercises/01.LargestCommonEnd/01.LargestCommonEnd/LargestCommonEnd.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class LargestCommonEnd
+{
+    public static void Main(string[] args)
+    {
+        string[] arrayOne = Console.ReadLine().Split(' ');
+        string[] arrayTwo = Console.ReadLine().Split(' ');
+
+        int leftCounter = CheckArray(arrayOne, arrayTwo);
+        Array.Reverse(arrayOne);
+        Array.Reverse(arrayTwo);
+        int rightCounter = CheckArray(arrayOne, arrayTwo);
+
+        Console.WriteLine(Math.Max(leftCounter, rightCounter));
+    }
+
+    private static int CheckArray(string[] arrOne, string[] arrTwo)
+    {
+        int minArray = Math.Min(arrOne.Length, arrTwo.Length);
+        int count = 0;
+
+        for (int i = 0; i < minArray; i++)
+        {
+            if (arrOne[i] == arrTwo[i])
+            {
+                count++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return count;
+    }
+}
