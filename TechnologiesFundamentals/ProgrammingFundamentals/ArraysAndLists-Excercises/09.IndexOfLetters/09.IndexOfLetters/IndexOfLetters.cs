288dd9295b7cc576382acc6bb886c71e7b26a7b1
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-30 23:03:32 +0200

    Create IndexOfLetters

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ArraysAndLists-Excercises/09.IndexOfLetters/09.IndexOfLetters/IndexOfLetters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class IndexOfLetters
+{
+    public static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            Console.WriteLine($"{input[i]} -> {input[i] - 'a'}");
+        }
+    }
+}
