c1e8ec4c292b94626aa62bae27d05a3488b0779a
stoyanov7 <georgi.stoyanov777@gmail.com>
2017-01-30 20:25:09 +0200

    Create SortNumbers

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/Lists-Lab/05.SortNumbers/05.SortNumbers/SortNumbers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SortNumbers
+{
+    public static void Main(string[] args)
+    {
+        var numberList = Console.ReadLine().Split(' ').Select(decimal.Parse).ToList();
+        numberList.Sort();
+
+        Console.WriteLine(string.Join(" <= ", numberList));
+    }
+}
