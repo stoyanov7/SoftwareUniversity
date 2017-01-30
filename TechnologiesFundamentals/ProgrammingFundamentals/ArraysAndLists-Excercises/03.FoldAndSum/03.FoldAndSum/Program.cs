fc44afceb89d7627b15b7df2b7eb98ba95303f76
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-30 22:58:06 +0200

    Create FoldAndSum

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ArraysAndLists-Excercises/03.FoldAndSum/03.FoldAndSum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int[] arrFoldA = new int[array.Length / 2];
+        int[] arrFoldB = new int[array.Length / 2];
+        int n = 0;
+
+        for (int i = array.Length / 4 - 1; i >= 0; i--)
+        {
+            arrFoldA[n] = array[i];
+            n++;
+        }
+        n = array.Length / 2 - 1;
+
+        for (int i = array.Length - array.Length / 4; i < array.Length; i++)
+        {
+            arrFoldA[n] = array[i];
+            n--;
+        }
+        n = 0;
+
+        int start = array.Length / 4;
+        int end = array.Length - array.Length / 4;
+
+        for (int i = start; i < end; i++)
+        {
+            arrFoldB[n] = array[i];
+            n++;
+        }
+
+        int[] sum = new int[arrFoldA.Length];
+
+        for (int i = 0; i < arrFoldA.Length; i++)
+        {
+            sum[i] = arrFoldA[i] + arrFoldB[i];
+        }
+
+        Console.WriteLine(string.Join(" ", sum));
+    }
+}
