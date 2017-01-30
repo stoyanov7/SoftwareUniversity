90d7592b003d8b58aa638127b280a2bb6fb99632
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-30 23:02:40 +0200

    Create MaxSequenceOfIncreasingElements

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ArraysAndLists-Excercises/07.MaxSequenceOfIncreasingElements/07.MaxSequenceOfIncreasingElements/MaxSequenceOfIncreasingElements.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+public class MaxSequenceOfIncreasingElements
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int length = numbers.Length;
+
+        MaxIncSequence(numbers, length);
+    }
+
+    static void MaxIncSequence(int[] numbers, int length)
+    {
+        int countCurrentSequence = 0;
+        int countMaxSequence = 0;
+        int startCurrentSequence = 0;
+        int startMaxSequence = 0;
+
+        for (int i = 1; i < length; i++)
+        {
+            if (numbers[i] - numbers[i - 1] >= 1)
+            {
+                countCurrentSequence++;
+                startCurrentSequence = i - countCurrentSequence;
+
+                if (countCurrentSequence > countMaxSequence)
+                {
+                    countMaxSequence = countCurrentSequence;
+                    startMaxSequence = startCurrentSequence;
+                }
+            }
+            else
+            {
+                countCurrentSequence = 0;
+            }
+        }
+        for (int iWrite = startMaxSequence; iWrite <= (startMaxSequence + countMaxSequence); iWrite++)
+        {
+            Console.Write(numbers[iWrite] + " ");
+        }
+        Console.WriteLine();
+    }
+}
