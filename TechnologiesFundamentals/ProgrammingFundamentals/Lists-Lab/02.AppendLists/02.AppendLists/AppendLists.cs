56455c2b54b15b63d193d0742ec43e78bc0bbe33
stoyanov7 <georgi.stoyanov777@gmail.com>
2017-01-30 20:23:54 +0200

    Create AppendLists

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/Lists-Lab/02.AppendLists/02.AppendLists/AppendLists.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class AppendLists
+{
+    public static void Main(string[] args)
+    {
+        var input = Console.ReadLine().Split('|').ToList();
+        input.Reverse();
+        var result = new List<string>();
+
+        foreach (var lines in input)
+        {
+            var numbers = lines.Split(' ').ToList();
+
+            foreach (var number in numbers)
+            {
+                if (number != "")
+                {
+                    result.Add(number);
+                }
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
