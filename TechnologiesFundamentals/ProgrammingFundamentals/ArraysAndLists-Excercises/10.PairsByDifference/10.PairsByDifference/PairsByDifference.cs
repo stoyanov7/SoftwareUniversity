e8f6a79e89398e83bfdafc6b57b342b6b5383e5e
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-30 23:03:56 +0200

    Create PairsByDifference

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ArraysAndLists-Excercises/10.PairsByDifference/10.PairsByDifference/PairsByDifference.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+public class PairsByDifference
+{
+    public static void Main(string[] args)
+    {
+        var elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        var number = int.Parse(Console.ReadLine());
+        var found = 0;
+
+        for (int i = 0; i < elements.Length; i++)
+        {
+            for (int j = elements.Length - 1; j > i; j--)
+            {
+                if (elements[i] - elements[j] == number || elements[j] - elements[i] == number)
+                {
+                    found++;
+                }
+            }
+        }
+
+        Console.WriteLine(found);
+    }
+}
