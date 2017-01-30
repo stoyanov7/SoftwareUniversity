77f9cb87778555f360b35054a714368bc3c77406
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-01-30 23:03:12 +0200

    Create MostFrequentNumber

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ArraysAndLists-Excercises/08.MostFrequentNumber/08.MostFrequentNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+public class MostFrequentNumber
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int maxFrequentNumber = 0;
+        int maxCounter = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            int counter = 0;
+
+            for (int j = i; j < numbers.Length; j++)
+            {
+                if (numbers[i] == numbers[j])
+                {
+                    counter++;
+
+                    if (counter > maxCounter)
+                    {
+                        maxCounter = counter;
+                        maxFrequentNumber = numbers[i];
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine(maxFrequentNumber);
+    }
+}
