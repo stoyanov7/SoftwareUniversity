d2efb294dcb798d9961460fa7310ccfaacfc3e5b
stoyanov7 <georgi.stoyanov777@gmail.com>
2017-01-30 20:24:25 +0200

    Create SumAdjacentEqualNumbers

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/Lists-Lab/03.SumAdjacentEqualNumbers/03.SumAdjacentEqualNumbers/SumAdjacentEqualNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class SumAdjacentEqualNumbers
+{
+    public static void Main(string[] args)
+    {
+        var numberList = Console.ReadLine().Split(' ').Select(decimal.Parse).ToList();
+
+        for (var i = 0; i < numberList.Count - 1; i++)
+        {
+            if (numberList[i] == numberList[i + 1])
+            {
+                numberList[i] = numberList[i] + numberList[i + 1];
+                numberList.RemoveAt(i + 1);
+                i = -1;
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", numberList));
+    }
+}
