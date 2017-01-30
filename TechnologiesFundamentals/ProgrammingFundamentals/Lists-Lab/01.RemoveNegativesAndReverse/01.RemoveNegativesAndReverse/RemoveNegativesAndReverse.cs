410613fdaac9bc2c334f5c0260bd84f879713d6b
stoyanov7 <georgi.stoyanov777@gmail.com>
2017-01-30 20:23:16 +0200

    Create RemoveNegativesAndReverse

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/Lists-Lab/01.RemoveNegativesAndReverse/01.RemoveNegativesAndReverse/RemoveNegativesAndReverse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class RemoveNegativesAndReverse
+{
+    public static void Main(string[] args)
+    {
+        var numberList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        numberList.RemoveAll(x => x < 0);
+        numberList.Reverse();
+
+        if (numberList.Count <= 0)
+        {
+            Console.WriteLine("empty");
+        }
+        else
+        {
+            Console.WriteLine(string.Join(" ", numberList));
+        }
+    }
+}
