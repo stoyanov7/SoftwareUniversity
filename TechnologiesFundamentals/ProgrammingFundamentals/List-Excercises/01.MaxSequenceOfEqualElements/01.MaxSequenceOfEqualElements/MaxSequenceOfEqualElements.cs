4a9c912838d3488c5fab07ac5da6fa7bd326ef5c
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-01 18:49:27 +0200

    Create MaxSequenceOfEqualElements

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/List-Excercises/01.MaxSequenceOfEqualElements/01.MaxSequenceOfEqualElements/MaxSequenceOfEqualElements.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+public class MaxSequenceOfEqualElements
+{
+    public static void Main(string[] args)
+    {
+        var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        var maxNumbers = 0;
+        var count = 1;
+        var maxCount = 1;
+
+        for (int i = 0; i < numbers.Count - 1; i++)
+        {
+            if (numbers[i] == numbers[i + 1])
+            {
+                count++;
+
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                    maxNumbers = numbers[i];
+                }
+            }
+            else
+            {
+                count = 1;
+            }
+
+            if (maxCount == 1)
+            {
+                maxNumbers = numbers[0];
+            }
+        }
+
+        for (int i = 0; i < maxCount; i++)
+        {
+            Console.Write(maxNumbers);
+            Console.Write(" ");
+        }
+    }
+}
