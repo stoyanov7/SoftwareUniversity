9aa4adfb6c53b40c97d3a947abf92aebe9752c35
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-01 11:30:02 +0200

    Create RectangleOfStars

---
+++ .ProgramingBasics/DrawingFiguresWithLoops/02.RectangleOfNxNStars/src/RectangleOfStars.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class RectangleOfStars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(repeatStr("*", n));
+        }
+    }
+
+    private static String repeatStr(String str, int count) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < count; i++) {
+            sb.append(str);
+        }
+
+        return sb.toString();
+    }
+}
