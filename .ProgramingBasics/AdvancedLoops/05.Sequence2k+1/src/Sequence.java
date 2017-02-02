1fb75f3d346825e147b2a44fc70541b84dd1749c
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-02 10:37:19 +0200

    Create Sequence2k+1

---
+++ .ProgramingBasics/AdvancedLoops/05.Sequence2k+1/src/Sequence.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Sequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        int k = 1;
+
+        while (k <= n) {
+            System.out.println(k);
+            k = 2 * k + 1;
+        }
+    }
+}
