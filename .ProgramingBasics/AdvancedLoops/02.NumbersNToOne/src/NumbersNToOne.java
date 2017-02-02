981c4a09be0dadb1dd03ed1f6300c5d85ebb6bde
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-02 10:36:09 +0200

    Create NumbersNToOne

---
+++ .ProgramingBasics/AdvancedLoops/02.NumbersNToOne/src/NumbersNToOne.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class NumbersNToOne {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = n; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
