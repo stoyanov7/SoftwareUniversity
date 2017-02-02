05690b934f21a97f302a2dd7ddae2cbc9bbaa092
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-03 00:10:46 +0200

    Create CountRealNumbers

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DictionariesLambdaAndLINQ-Lab/01.CountRealNumbers/01.CountRealNumbers/CountRealNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CountRealNumbers
+{
+    public static void Main(string[] args)
+    {
+        var inputNumbers = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+        var countsDictionary = new SortedDictionary<double, int>();
+
+        foreach (var number in inputNumbers)
+        {
+            if (!countsDictionary.ContainsKey(number))
+            {
+                countsDictionary[number] = 0;
+            }
+
+            countsDictionary[number]++;
+        }
+
+        foreach (var element in countsDictionary)
+        {
+            Console.WriteLine($"{element.Key} -> {element.Value}");
+        }
+    }
+}
