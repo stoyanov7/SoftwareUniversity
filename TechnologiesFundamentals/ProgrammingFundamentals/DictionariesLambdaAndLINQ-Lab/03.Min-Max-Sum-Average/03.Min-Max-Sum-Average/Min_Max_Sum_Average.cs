7db70180438a26ba6eb475bf2343d099d2ca4556
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-03 00:11:56 +0200

    Create Min-Max-Sum-Average

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DictionariesLambdaAndLINQ-Lab/03.Min-Max-Sum-Average/03.Min-Max-Sum-Average/Min_Max_Sum_Average.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+public class Min_Max_Sum_Average
+{
+    public static void Main(string[] args)
+    {
+        var n = int.Parse(Console.ReadLine());
+        var inputNumbers = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            inputNumbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine($"Sum = {inputNumbers.Sum()}");
+        Console.WriteLine($"Min = {inputNumbers.Min()}");
+        Console.WriteLine($"Max = {inputNumbers.Max()}");
+        Console.WriteLine($"Average = {inputNumbers.Average()}");
+    }
+}
