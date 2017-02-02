b9687730e638fbe45ee5cdf4aba8c483de4a7b50
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-03 00:12:36 +0200

    Create ShortWordsSorted

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DictionariesLambdaAndLINQ-Lab/05.ShortWordsSorted/05.ShortWordsSorted/ShortWordsSorted.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+public class ShortWordsSorted
+{
+    public static void Main(string[] args)
+    {
+        var separators = new char[]
+        {
+            ' ', '.', ',', ':', ';', '(', ')', '[', ']', '\"', '\'', '/', '\\', '!', '?'
+        };
+
+        var input = Console.ReadLine()
+            .ToLower()
+            .Split(separators, StringSplitOptions.RemoveEmptyEntries)
+            .Where(w => w.Length < 5)
+            .OrderBy(w => w)
+            .Distinct()
+            .ToList();
+
+        Console.WriteLine(string.Join(", ", input));
+    }
+}
