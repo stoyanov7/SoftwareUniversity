8e71d24a9ab48a87e53248ad867ab431b430dce9
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-03 00:11:10 +0200

    Create OddOccurrences

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DictionariesLambdaAndLINQ-Lab/02.OddOccurrences/02.OddOccurrences/OddOccurrences.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class OddOccurrences
+{
+    public static void Main(string[] args)
+    {
+        var inputWords = Console.ReadLine().ToLower().Split(' ').ToArray();
+        var wordCount = new Dictionary<string, int>();
+
+        foreach (var word in inputWords)
+        {
+            if (!wordCount.ContainsKey(word))
+            {
+                wordCount[word] = 0;
+            }
+
+            wordCount[word]++;
+        }
+
+        var result = new List<string>();
+
+        foreach (var element in wordCount)
+        {
+            if (element.Value % 2 != 0)
+            {
+                result.Add(element.Key);
+            }
+        }
+
+        Console.WriteLine(string.Join(", ", result));
+    }
+}
