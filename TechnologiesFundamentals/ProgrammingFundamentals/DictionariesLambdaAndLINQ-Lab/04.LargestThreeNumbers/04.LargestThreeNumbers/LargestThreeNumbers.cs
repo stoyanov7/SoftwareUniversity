4af14d27803a2be1ab7fc4ebae66521b2b760ef4
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-03 00:12:16 +0200

    Create LargestThreeNumbers

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DictionariesLambdaAndLINQ-Lab/04.LargestThreeNumbers/04.LargestThreeNumbers/LargestThreeNumbers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+public class LargestThreeNumbers
+{
+    public static void Main(string[] args)
+    {
+        var inputNumbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        var largestThreeNumbers = inputNumbers.OrderByDescending(n => n).Take(3);
+
+        Console.WriteLine(string.Join(", ", largestThreeNumbers));
+    }
+}
