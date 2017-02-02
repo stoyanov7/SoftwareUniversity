ba8d9df030be462ec3917a910792e68b28595d6c
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-02 10:36:34 +0200

    Create PowersОfTwo

---
+++ .ProgramingBasics/AdvancedLoops/03.PowersОfTwo/src/PowersОfTwo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PowersОfTwo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i <= n; i++) {
+            System.out.println((int)Math.pow(2, i));
+        }
+    }
+}
