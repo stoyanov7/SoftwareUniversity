aee70573530a78982bbcfb9a79605c6051f4352e
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-05 13:22:50 +0200

    Create Phonebook

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DictionariesLambdaAndLINQ-Excercises/01.Phonebook/01.Phonebook/Phonebook.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Phonebook
+{
+    public static void Main(string[] args)
+    {
+        var input = Console.ReadLine()
+            .Split(' ')
+           .ToArray();
+
+        var phonebook = new Dictionary<string, string>();
+        
+
+        while (!input[0].Equals("END"))
+        {
+            var command = input[0];
+            var name = input[1];
+
+            switch (command)
+            {
+                case "A":
+                    var telephoneNumber = input[2];
+                    phonebook[name] = telephoneNumber;
+                    break;
+                     
+                case "S":
+                    if (phonebook.ContainsKey(name))
+                    {
+                        Console.WriteLine("{0} -> {1}", name, phonebook[name]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Contact {0} does not exist.", name);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            input = Console.ReadLine().Split(' ');
+        }
+    }
+}
