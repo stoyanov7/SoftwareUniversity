b7e81211c796d00e3024acdb5f020a7d778bc8ea
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-05 13:23:58 +0200

    Create A Miner Task

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DictionariesLambdaAndLINQ-Excercises/03.AMinerTask/03.AMinerTask/AMinerTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class AMinerTask
+{
+    public static void Main(string[] args)
+    {
+        var products = new Dictionary<string, decimal>();
+        var input = Console.ReadLine();
+
+        while (!input.Equals("stop"))
+        {
+            var resource = input;
+            var quantity = decimal.Parse(Console.ReadLine());
+
+            if (!products.ContainsKey(resource))
+            {
+                products[resource] = 0;
+            }
+
+            products[resource] += quantity;
+
+            input = Console.ReadLine();
+        }
+
+        foreach (var element in products)
+        {
+            Console.WriteLine($"{element.Key} -> {element.Value}");
+        }
+    }
+}
