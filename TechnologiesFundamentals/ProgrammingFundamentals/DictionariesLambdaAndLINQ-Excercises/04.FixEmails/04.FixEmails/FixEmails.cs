2ab186944eff799e69151cde2362a961be4a33ee
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-05 13:24:22 +0200

    Create Fix Emails

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DictionariesLambdaAndLINQ-Excercises/04.FixEmails/04.FixEmails/FixEmails.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class FixEmails
+{
+    public static void Main(string[] args)
+    {
+        var nameAndEmail = new Dictionary<string, string>();
+
+        while (true)
+        {
+            var name = Console.ReadLine();
+
+            if (name.Equals("stop"))
+            {
+                break;
+            }
+
+            var email = Console.ReadLine();
+
+            if (!nameAndEmail.ContainsKey(name) && !(email.EndsWith("uk") || email.EndsWith("us")))
+            {
+                nameAndEmail[name] = email;
+            }
+        }
+
+        foreach (var element in nameAndEmail)
+        {
+            Console.WriteLine($"{element.Key} -> {element.Value}");
+        }
+    }
+}
