ef7c00bc3047311d8b4e5cbbb6f4e32becda11ab
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-07 13:26:09 +0200

    Create RandomizeWords

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ObjectsAndClasses-Lab/02.RandomizeWords/02.RandomizeWords/RandomizeWords.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class RandomizeWords
+{
+    public static void Main(string[] args)
+    {
+        var words = Console.ReadLine().Split(' ');
+        var random = new Random();
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            var currentWord = words[i];
+            var randomIndex = random.Next(0, words.Length);
+
+            var temp = words[randomIndex];
+            words[i] = temp;
+            words[randomIndex] = currentWord;
+        }
+
+        foreach (var word in words)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
