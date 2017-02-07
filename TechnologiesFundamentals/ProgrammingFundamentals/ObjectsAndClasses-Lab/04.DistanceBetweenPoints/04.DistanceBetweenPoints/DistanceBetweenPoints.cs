d2889659cd4a301338caea6e1d14754fafbfcef8
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-07 13:26:48 +0200

    Create  DistanceBetweenPoints

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ObjectsAndClasses-Lab/04.DistanceBetweenPoints/04.DistanceBetweenPoints/DistanceBetweenPoints.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+public class DistanceBetweenPoints
+{
+    public static void Main(string[] args)
+    {
+        var firstPont = ReadPoint();
+        var secondPoint = ReadPoint();
+
+        var result = CalculateDistance(firstPont, secondPoint);
+
+        Console.WriteLine($"{result:F3}");
+    }
+
+    public static Point ReadPoint()
+    {
+        var inputCoordinats = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        var point = new Point
+        {
+            X = inputCoordinats[0],
+            Y = inputCoordinats[1]
+        };
+
+        return point;
+    }
+
+    public static double CalculateDistance(Point firstPoint, Point secondPoint)
+    {
+        var diffX = firstPoint.X - secondPoint.X;
+        var diffY = firstPoint.Y - secondPoint.Y;
+
+        var powX = Math.Pow(diffX, 2);
+        var powY = Math.Pow(diffY, 2);
+
+        return Math.Sqrt(powX + powY);
+    }
+}
---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ObjectsAndClasses-Lab/04.DistanceBetweenPoints/04.DistanceBetweenPoints/Point.cs
@@ -0,0 +1,7 @@
+﻿public class Point
+{
+    public double X { get; set; }
+
+    public double Y { get; set; }
+
+}
