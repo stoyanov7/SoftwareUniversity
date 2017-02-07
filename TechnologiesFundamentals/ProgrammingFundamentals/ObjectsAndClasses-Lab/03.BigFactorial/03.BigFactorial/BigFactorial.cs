e7177ebffc891b6de2c667f74a879fbb9fe8088b
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-07 13:26:28 +0200

    Create BigFactorial

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ObjectsAndClasses-Lab/03.BigFactorial/03.BigFactorial/BigFactorial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+public class BigFactorial
+{
+    public static void Main(string[] args)
+    {
+        var num = int.Parse(Console.ReadLine());
+
+        BigInteger factorial = 1;
+
+        for (int i = 1; i <= num; i++)
+        {
+            factorial = factorial * i;
+        }
+
+        Console.WriteLine(factorial);
+    }
+}
