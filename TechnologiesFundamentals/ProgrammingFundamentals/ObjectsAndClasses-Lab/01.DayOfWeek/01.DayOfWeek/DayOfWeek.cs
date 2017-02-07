db1f5250238a37d2bd5a944d303d9827d343a943
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-07 13:25:28 +0200

    Create DayOfWeek

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ObjectsAndClasses-Lab/01.DayOfWeek/01.DayOfWeek/DayOfWeek.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+public class DayOfWeek
+{
+    public static void Main(string[] args)
+    {
+        DateTime dateTime = DateTime.ParseExact(Console.ReadLine(),
+            "d-M-yyyy", CultureInfo.InvariantCulture);
+
+        Console.WriteLine(dateTime.DayOfWeek);
+    }
+}
