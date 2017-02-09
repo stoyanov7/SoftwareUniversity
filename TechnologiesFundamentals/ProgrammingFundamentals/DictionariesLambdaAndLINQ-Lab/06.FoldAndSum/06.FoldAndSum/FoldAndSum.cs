70e231832133619cb7d7c4672b59ad3c7c0fe61c
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-09 12:06:32 +0200

    Update

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DictionariesLambdaAndLINQ-Lab/06.FoldAndSum/06.FoldAndSum/FoldAndSum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+public class FoldAndSum
+{
+    public static void Main(string[] args)
+    {
+        var inputNumbers = Console.ReadLine()
+            .Split(' ')
+            .Select(int.Parse)
+            .ToArray();
+
+        var k = inputNumbers.Length / 4;
+
+        var firstRowLeft = inputNumbers
+            .Take(3)
+            .Reverse()
+            .ToArray();
+
+        var firstRowRigth = inputNumbers
+            .Reverse()
+            .Take(3)
+            .ToArray();
+
+        var firstRow = firstRowLeft
+            .Concat(firstRowRigth)
+            .ToArray();
+
+        var secondRow = inputNumbers
+            .Skip(k)
+            .Take(2 * k);
+
+        firstRow
+            .Zip(secondRow, (x, y) => x + y)
+            .ToList()
+            .ForEach(Console.WriteLine);
+    }
+}
