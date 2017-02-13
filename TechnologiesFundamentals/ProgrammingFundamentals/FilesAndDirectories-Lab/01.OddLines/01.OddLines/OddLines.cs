3b07eba6d2c547944ded6afa3020521189f38f03
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-13 19:43:08 +0200

    Create OddLines

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/FilesAndDirectories-Lab/01.OddLines/01.OddLines/OddLines.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class OddLines
+{
+    public static void Main(string[] args)
+    {
+        var inputFile = "Input.txt";
+        var resultFile = "Result.txt";
+        var inputText = File.ReadAllLines(inputFile);
+
+        if (!File.Exists(resultFile))
+        {
+            File.Create(resultFile);
+        }
+
+        var oddLines = new List<string>();
+
+        for (int i = 0; i < inputText.Length; i++)
+        {
+            if (i % 2 != 0)
+            {
+                oddLines.Add(inputText[i]);
+            }
+        }
+
+        File.WriteAllLines(resultFile, oddLines);
+    }
+}
