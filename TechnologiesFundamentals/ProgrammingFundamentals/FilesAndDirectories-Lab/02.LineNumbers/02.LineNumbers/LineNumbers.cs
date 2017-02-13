ba66841b8be4cad1801e8152f7dcd91cf21aeb07
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-13 19:45:31 +0200

    Create LineNumbers

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/FilesAndDirectories-Lab/02.LineNumbers/02.LineNumbers/LineNumbers.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+public class LineNumbers
+{
+    public static void Main()
+    {
+        var inputText = "input.txt";
+        var outputText = "output.txt";
+
+        var inputFile = File.ReadAllLines(inputText);
+        var result = new List<string>();
+
+        int count = 1;
+        foreach (var line in inputFile)
+        {
+            result.Add($"{count}. {line}");
+            count++;
+        }
+
+        File.WriteAllLines(outputText, result);
+    }
+}
