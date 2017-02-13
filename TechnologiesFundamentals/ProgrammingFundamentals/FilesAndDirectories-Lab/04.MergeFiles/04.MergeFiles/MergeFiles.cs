874f4cc2d1ffb6541238f7ca7cc265673d5dde88
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-13 19:51:59 +0200

    Create MergeFiles

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/FilesAndDirectories-Lab/04.MergeFiles/04.MergeFiles/MergeFiles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class MergeFiles
+{
+    public static void Main(string[] args)
+    {
+        var inputOneTxt = "FileOne.txt";
+        var inputTwoTxt = "FileTwo.txt";
+        var outputTxt = "Result.txt";
+
+        List<string> textOne = File.ReadAllLines(inputOneTxt).ToList();
+        List<string> textTwo = File.ReadAllLines(inputTwoTxt).ToList();
+
+        textOne.AddRange(textTwo);
+        textOne.Sort();
+
+        File.AppendAllLines(outputTxt, textOne);
+    }
+}
