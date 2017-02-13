07ba861a38efe974596348c0abf3c5a2ceadc010
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-13 19:54:09 +0200

    Create FolderSize

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/FilesAndDirectories-Lab/05.FolderSize/05.FolderSize/FolderSize.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+public class FolderSize
+{
+    public static void Main(string[] args)
+    {
+        var folder = "TestFolder";
+        var outputTxt = "Result.txt";
+        var inputFile = Directory.GetFiles(folder);
+        double sum = 0;
+
+        foreach (var file in inputFile)
+        {
+            FileInfo fileInfo = new FileInfo(file);
+            sum += fileInfo.Length;
+        }
+        sum = sum / 1024.0 / 1024.0;
+
+        File.WriteAllText(outputTxt, sum.ToString());
+    }
+}
