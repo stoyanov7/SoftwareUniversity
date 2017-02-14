f96d5497cadc39fadced28d9ce7bd463b4f8ee2c
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-14 11:37:05 +0200

    Create ReverseString

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/Strings-Lab/01.ReverseString/01.ReverseString/ReverseString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class ReverseString
+{
+    public static void Main(string[] args)
+    {
+        var input = Console.ReadLine();
+        Console.WriteLine(Reverse(input));
+    }
+
+    public static string Reverse(string s)
+    {
+        var array = s.ToCharArray();
+        Array.Reverse(array);
+
+        return new string(array);
+    }
+}
