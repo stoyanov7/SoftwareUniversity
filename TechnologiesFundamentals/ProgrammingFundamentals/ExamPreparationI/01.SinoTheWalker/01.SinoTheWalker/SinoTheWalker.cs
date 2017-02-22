c4f5fe7fed3de4b60c90dd9300552a49e1c33735
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-22 18:12:36 +0200

    Create SinoTheWalker

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ExamPreparationI/01.SinoTheWalker/01.SinoTheWalker/SinoTheWalker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+public class SinoTheWalker
+{
+    public static void Main(string[] args)
+    {
+        DateTime leaves = DateTime.ParseExact(Console.ReadLine(), "HH:mm:ss", CultureInfo.InvariantCulture);
+        int numberOfSteps = int.Parse(Console.ReadLine()) % 86400;
+        int timeForSteps = int.Parse(Console.ReadLine()) % 86400;
+        int walkSeconds = numberOfSteps * timeForSteps;
+        leaves = leaves.AddSeconds(walkSeconds);
+
+        Console.WriteLine($"Time Arrival: {leaves.ToString("HH:mm:ss")}");
+    }
+}
