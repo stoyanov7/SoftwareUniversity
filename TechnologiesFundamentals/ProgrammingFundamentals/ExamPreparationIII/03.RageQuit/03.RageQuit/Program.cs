9feb867e25527c5de390879d2d0a7499a71295ef
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-23 16:45:24 +0200

    Create RageQuit

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ExamPreparationIII/03.RageQuit/03.RageQuit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class RageQuit
+{
+    public static void Main(string[] args)
+    {
+        var input = Console.ReadLine().ToUpper();
+        var patern = @"(\D+)(\d+)";
+
+        var regex = new Regex(patern);
+        var collection = regex.Matches(input);
+        var count = 0;
+        var output = new StringBuilder();
+
+        foreach (Match match in collection)
+        {
+            for (var i = 0; i < int.Parse(match.Groups[2].ToString()); i++)
+            {
+                output.Append(match.Groups[1]);
+            }
+        }
+
+        count = output.ToString().Distinct().Count();
+
+        Console.WriteLine($"Unique symbols used: {count}");
+        Console.WriteLine($"{output}");
+    }
+}
