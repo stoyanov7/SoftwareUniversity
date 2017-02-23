dd6298633369ddf8a70b0832fa5629ea53ab354c
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-23 16:44:30 +0200

    Create SoftuniCoffeeOrders

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ExamPreparationIII/01.SoftuniCoffeeOrders/01.SoftuniCoffeeOrders/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+public class SoftuniCoffeeOrders
+{
+    public static void Main(string[] args)
+    {
+        var countOfOrders = int.Parse(Console.ReadLine());
+        var totalPrice = 0m;
+
+        for (int i = 0; i < countOfOrders; i++)
+        {
+            var pricePerCapsule = decimal.Parse(Console.ReadLine());
+            var orderDate = DateTime.ParseExact(Console.ReadLine(), "d/M/yyyy", CultureInfo.InvariantCulture);
+            var capsulesCount = long.Parse(Console.ReadLine());
+
+            var daysInMonth = DateTime.DaysInMonth(orderDate.Year, orderDate.Month);
+            var price = ((daysInMonth * capsulesCount) * pricePerCapsule);
+            Console.WriteLine($"The price for the coffee is: ${price:F2}");
+            totalPrice += price;
+        }
+
+        Console.WriteLine($"Total: ${totalPrice:F2}");
+    }
+}
