9e1b2b9b5353d751a6acf36b675dbaa3b6abe014
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-02-26 12:27:24 +0200

    Create SweetDesert

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/ExamPreparationIV/01.SweetDesert/01.SweetDesert/SweetDesert.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class SweetDesert
+{
+    public static void Main(string[] args)
+    {
+        var amountOfCash = decimal.Parse(Console.ReadLine());
+        var numberOfGuests = long.Parse(Console.ReadLine());
+        var priceOfBananas = decimal.Parse(Console.ReadLine());
+        var priceOfEggs = decimal.Parse(Console.ReadLine());
+        var priceOfBerries = decimal.Parse(Console.ReadLine());
+
+        var portionsCount = (int)Math.Ceiling(numberOfGuests / 6.0);
+
+        var total = (portionsCount * 2 * priceOfBananas) + (portionsCount * 4 * priceOfEggs) + (portionsCount * 0.2m * priceOfBerries);
+
+        if (total <= amountOfCash)
+        {
+            Console.WriteLine($"Ivancho has enough money - it would cost {total:F2}lv.");
+        }
+        else
+        {
+            Console.WriteLine($"Ivancho will have to withdraw money - he will need {(total - amountOfCash):F2}lv more.");
+        }
+    }
+}
