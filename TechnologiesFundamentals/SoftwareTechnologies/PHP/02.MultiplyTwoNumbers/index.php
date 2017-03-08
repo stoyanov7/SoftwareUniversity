cc9ab5bac74707c722df0c06fc36549b95f12261
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-03-08 11:06:41 +0200

    Create Multiply Two N Number

---
+++ TechnologiesFundamentals/SoftwareTechnologies/PHP/02.MultiplyTwoNumbers/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>First Steps Into PHP</title>
+</head>
+
+<body>
+<form>
+    N: <input type="text" name="num1" />
+    M: <input type="text" name="num2" />
+    <input type="submit" />
+</form>
+
+<?php
+if (isset($_GET['num1']) && isset($_GET['num2'])) {
+    $n = intval($_GET['num1']);
+    $m = intval($_GET['num2']);
+    echo $n * $m;
+}
+?>
+
+</body>
+</html>
