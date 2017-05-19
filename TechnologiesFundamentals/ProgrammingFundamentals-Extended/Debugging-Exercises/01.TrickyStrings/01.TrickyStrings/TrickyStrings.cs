6089016dec7cb7b5bce0eb64ad0ebb6ff52979b2
stoyanov7 <georgi.stoyanov777@gmail.com>
2017-05-19 12:20:55 +0300

    Create Debugging Exercises

---
+++ TechnologiesFundamentals/ProgrammingFundamentals-Extended/Debugging-Exercises/01.TrickyStrings/01.TrickyStrings/TrickyStrings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01.TrickyStrings
+{
+    public class TrickyStrings
+    {
+        public static void Main(string[] args)
+        {
+            var delimeter = Console.ReadLine();
+            var numberOfStrings = int.Parse(Console.ReadLine());
+            var result = string.Empty;
+
+            for (var i = 0; i < numberOfStrings; i++)
+            {
+                if (i < numberOfStrings - 1)
+                {
+                    result += Console.ReadLine() + delimeter;
+                    continue;
+                }
+
+                result += Console.ReadLine();
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
