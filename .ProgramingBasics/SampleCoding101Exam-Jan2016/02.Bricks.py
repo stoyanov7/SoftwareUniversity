a1a039306251f043824d012a8a2185a4c4131c13
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-06-02 00:25:24 +0300

    Delete 02.Bricks.py

--- .ProgramingBasics/SampleCoding101Exam-Jan2016/02.Bricks.py
+++
@@ -1,9 +0,0 @@
-import math
-
-bricks = int(input())
-workers = int(input())
-bricksPerWorker = int(input())
-
-bricksPerWork = workers * bricksPerWorker
-coursesCount = math.ceil(bricks / bricksPerWork)
-print(coursesCount)
