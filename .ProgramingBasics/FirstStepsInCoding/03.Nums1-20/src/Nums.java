0df87d0e4b1e396bf4f0d3558cbc12c576474891
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-06-09 10:21:24 +0300

    Rename folder from .ProgramingBasics to ProgramingBasics

--- .ProgramingBasics/FirstStepsInCoding/03.Nums1-20/src/Nums.java
+++
@@ -1,7 +0,0 @@
-public class Nums {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 20; i++) {
-            System.out.println(i);
-        }
-    }
-}
--- .ProgramingBasics/Simple-Loops/03.LatinLetters/src/LatinLetters.java
+++
@@ -1,7 +0,0 @@
-public class LatinLetters {
-    public static void main(String[] args) {
-        for (char i = 'a'; i <= 'z'; i++) {
-            System.out.println(i);
-        }
-    }
-}
