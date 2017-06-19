3e71727ff34132522df0df7a57006617aa54e5d7
stoyanov7 <georgi.stoyanov777@gmail.com>
2017-06-19 16:14:29 +0300

    Create Dictionaries

---
+++ TechnologiesFundamentals/ProgrammingFundamentals-Extended/Dictionaries/01.CountRealNumbers/01.CountRealNumbers/CountRealNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.CountRealNumbers
+{
+    public class CountRealNumbers
+    {
+        public static void Main(string[] args)
+        {
+            var inputNumbers = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+
+            var countsDictionary = new SortedDictionary<double, int>();
+
+            foreach (var number in inputNumbers)
+            {
+                if (!countsDictionary.ContainsKey(number))
+                {
+                    countsDictionary[number] = 0;
+                }
+                
+                countsDictionary[number]++;
+            }
+            
+            foreach (var element in countsDictionary)
+            {
+                Console.WriteLine($"{element.Key} -> {element.Value}");
+            }
+        }
+    }
+}
---
+++ TechnologiesFundamentals/ProgrammingFundamentals-Extended/Dictionaries/02.OddOccurences/02.OddOccurences/OddOccurences.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.OddOccurences
+{
+    public class OddOccurences
+    {
+        public static void Main(string[] args)
+        {
+            var words = Console.ReadLine()
+                .ToLower()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (!result.ContainsKey(word))
+                {
+                    result[word] = 0;
+                }
+
+                result[word]++;
+            }
+
+            var oddNumbersWord = new List<string>();
+
+            foreach (var kvp in result)
+            {
+                var value = kvp.Value;
+
+                if (value % 2 != 0)
+                {
+                    oddNumbersWord.Add(kvp.Key);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", oddNumbersWord));
+        }
+    }
+}
