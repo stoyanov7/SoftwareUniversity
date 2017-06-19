45ac01d14f2a168470cb4cca2c69f1175fdd9043
stoyanov7 <georgi.stoyanov777@gmail.com>
2017-06-19 09:42:33 +0300

    Create 01.Min3Numbers

---
+++ SoftuniadaTrainingContest-Sandbox/01.Min3Numbers/01.Min3Numbers/Min3Numbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.Min3Numbers
+{
+    public class Min3Numbers
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var numbers = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+
+            numbers.Sort();
+
+            for (int i = 0; i < Math.Min(n, 3); i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
