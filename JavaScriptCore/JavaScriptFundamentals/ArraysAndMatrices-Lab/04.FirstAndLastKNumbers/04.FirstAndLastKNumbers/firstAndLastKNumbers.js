f3a89b4e8039414adb9baeec437e7b05f9a3885a
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-06-19 14:21:06 +0300

    Update firstAndLastKNumbers.js

--- JavaScriptCore/JavaScriptFundamentals/ArraysAndMatrices-Lab/04.FirstAndLastKNumbers/04.FirstAndLastKNumbers/firstAndLastKNumbers.js
+++ JavaScriptCore/JavaScriptFundamentals/ArraysAndMatrices-Lab/04.FirstAndLastKNumbers/04.FirstAndLastKNumbers/firstAndLastKNumbers.js
@@ -1,5 +1,5 @@
 function firstAndLastKNumbers(arr) {
     let k = Number(arr.shift());
     console.log(arr.slice(0, k).join(" "));
-    console.log(arr.slice(arr.length - k, arr.length).join(" "));
+    console.log(arr.slice(-k).join(" "));
 }
