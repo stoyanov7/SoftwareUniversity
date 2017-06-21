f27efb99e24947fa115334e9abbea4b44f5d0c4f
stoyanov7 <georgi.stoyanov777@gmail.com>
2017-06-21 11:53:41 +0300

    Create Simple Arrays - Exercises

---
+++ TechnologiesFundamentals/ProgrammingFundamentals-Extended/SimpleArrays-MoreExercises/03.Phonebook/03.Phonebook/Phonebook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Phonebook
+{
+    public class Phonebook
+    {
+        public static void Main(string[] args)
+        {
+            var phoneNumbers = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+
+            var names = Console.ReadLine()
+                .Split(' ')
+                .ToArray();
+
+            var name = Console.ReadLine();
+
+            while (name != "done")
+            {
+                for (int i = 0; i < names.Length; i++)
+                {
+                    if (name == names[i])
+                    {
+                        Console.WriteLine($"{names[i]} -> {phoneNumbers[i]}");
+                    }
+                }
+
+                name = Console.ReadLine();
+            }
+        }
+    }
+}
