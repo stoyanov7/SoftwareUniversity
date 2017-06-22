19b4a067f7cc0b628f6a36edbff71d7205c81c23
stoyanov7 <georgi.stoyanov777@gmail.com>
2017-06-22 11:14:20 +0300

    Create Regular Expressions - Lab

---
+++ TechnologiesFundamentals/RegularExpressions(RegEx)-Lab/01.ReplaceATag/01.ReplaceATag/ReplaceATag.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.ReplaceATag
+{
+    public class ReplaceATag
+    {
+        public static void Main(string[] args)
+        {
+            var line = Console.ReadLine();
+            var pattern = @"<a.*href=(""|')(.*)\1>(.*?)<\/a>";
+
+            while (line != "end")
+            {
+                var reg = new Regex(pattern);
+                var matches = reg.Matches(line);
+
+                line = reg.Replace(line, @"[URL href=""$2""]$3[/URL]");
+                Console.WriteLine(line);
+
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
