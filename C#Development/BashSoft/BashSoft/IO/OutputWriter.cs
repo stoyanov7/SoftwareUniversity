d4c64848c3c068f13c50fdf9c91440c47544ad3d
stoyanov7 <georgi.stoyanov777@gmail.com>
2017-07-05 15:43:43 +0300

    Add OutputWriter

---
+++ C#Development/BashSoft/BashSoft/IO/OutputWriter.cs
@@ -0,0 +1,21 @@
+﻿namespace BashSoft.IO
+{
+    using System;
+
+    public static class OutputWriter
+    {
+        public static void WriteMessage(string message) => Console.Write(message);
+
+        public static void WriteMessageOnNewLine(string message) => Console.WriteLine(message);
+
+        public static void WriteEmptyLine() => Console.WriteLine();
+
+        public static void DisplayException(string message)
+        {
+            ConsoleColor currentColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = currentColor;
+        }
+    }
+}
