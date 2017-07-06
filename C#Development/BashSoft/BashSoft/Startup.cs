0383e2162151ae52bd3aa4a0da1ff21cf1a8ee7d
stoyanov7 <georgi.stoyanov777@gmail.com>
2017-07-06 14:45:14 +0300

    Add method PrintMenu() to StartUp.cs

--- C#Development/BashSoft/BashSoft/Startup.cs
+++ C#Development/BashSoft/BashSoft/Startup.cs
@@ -1,17 +1,13 @@
 ﻿namespace BashSoft
 {
     using BashSoft.IO;
-    using BashSoft.Repositories;
 
     public class Startup
     {
         public static void Main(string[] args)
         {
             OutputWriter.PrintLogo();
-            //IOManager.TraverseDirectory(@"some path");
-            //StudentsRepository.InitializeData();
-            //StudentsRepository.GetAllStudentFromCourse("Unity");
-            //StudentsRepository.GetStudentFromCourse("Unity", "Ivan");
+            OutputWriter.PrintMenu();
         }
     }
 }
