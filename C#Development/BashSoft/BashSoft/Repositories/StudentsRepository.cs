9e4a6956ab907fe32ea1752e89a38c7fc4aad8e3
stoyanov7 <georgi.stoyanov777@gmail.com>
2017-07-06 13:31:28 +0300

    Create StudentRepository

---
+++ C#Development/BashSoft/BashSoft/Repositories/StudentsRepository.cs
@@ -0,0 +1,121 @@
+﻿namespace BashSoft.Repositories
+{
+    using BashSoft.Exceptions;
+    using BashSoft.IO;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class StudentsRepository
+    {
+        public static bool isDataInitialized = false;
+
+        private static Dictionary<string, Dictionary<string, List<int>>> studentsByCourse;
+
+        public static void InitializeData()
+        {
+            if (!isDataInitialized)
+            {
+                
+                OutputWriter.WriteMessageOnNewLine("Reading data...");
+                studentsByCourse = new Dictionary<string, Dictionary<string, List<int>>>();
+                ReadData();
+            }
+            else
+            {
+                OutputWriter.WriteMessageOnNewLine(ExceptionMessages.DataAlreadyInitialisedException);
+            }
+        }
+
+        private static void ReadData()
+        {
+            var input = Console.ReadLine();
+
+            while (!string.IsNullOrEmpty(input))
+            {
+
+                //TODO: Split input and initilize variables
+                var inputTokens = input
+                    .Split(' ')
+                    .ToArray();
+
+                var course = inputTokens[0];
+                var student = inputTokens[1];
+                var mark = int.Parse(inputTokens[2]);
+
+                //TODO: Add the cource and the student if they don't exist
+                if (!studentsByCourse.ContainsKey(course))
+                {
+                    studentsByCourse.Add(course, new Dictionary<string, List<int>>());
+                }
+
+                if (!studentsByCourse[course].ContainsKey(student))
+                {
+                    studentsByCourse[course].Add(student, new List<int>());
+                }
+
+                //TODO: Add mark
+                studentsByCourse[course][student].Add(mark);
+
+                input = Console.ReadLine(); 
+            }
+
+            isDataInitialized = true;
+            OutputWriter.WriteMessageOnNewLine("Data read!");
+        }
+
+        private static bool IsQueryForCoursePossible(string courseName)
+        {
+            if (isDataInitialized)
+            {
+                if (studentsByCourse.ContainsKey(courseName))
+                {
+                    return true;
+                }
+
+                OutputWriter.DisplayException(ExceptionMessages.InexistingCourseInDataBase);
+            }
+            else
+            {
+                OutputWriter.DisplayException(ExceptionMessages.DataNotInitializedExceptionMessage);
+            }
+
+            return false;
+        }
+
+        private static bool IsQueryForStudentPossiblе(string courseName, string studentUserName)
+        {
+            if (IsQueryForCoursePossible(courseName) && studentsByCourse[courseName].ContainsKey(studentUserName))
+            {
+                return true;
+            }
+            else
+            {
+                OutputWriter.DisplayException(ExceptionMessages.InexistingStudentInDataBase);
+            }
+
+            return false;
+        }
+
+        public static void GetStudentFromCourse(string courseName, string username)
+        {
+            if (IsQueryForStudentPossiblе(courseName, username))
+            {
+                OutputWriter.PrintStudent(new KeyValuePair<string, List<int>>(username, studentsByCourse[courseName][username]));
+            }
+        }
+
+        public static void GetAllStudentFromCourse(string courseName)
+        {
+            if (IsQueryForCoursePossible(courseName))
+            {
+                OutputWriter.WriteMessageOnNewLine($"{courseName}:");
+
+                foreach (var studentMarkEntry in studentsByCourse[courseName])
+                {
+                    OutputWriter.PrintStudent(studentMarkEntry);
+                }
+            }
+        }
+    }
+}
