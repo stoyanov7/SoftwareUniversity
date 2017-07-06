629b0b24fc3990346792359d138b95a5200348fc
stoyanov7 <georgi.stoyanov777@gmail.com>
2017-07-06 14:43:27 +0300

    New exception - DirectoryNotFound

--- C#Development/BashSoft/BashSoft/Exceptions/ExceptionMessages.cs
+++ C#Development/BashSoft/BashSoft/Exceptions/ExceptionMessages.cs
@@ -9,5 +9,7 @@
         public const string InexistingCourseInDataBase = "The course you are trying to get does not exist in the data base!";
 
         public const string InexistingStudentInDataBase = "The user name for the student you are trying to get does not exist!";
+
+        public const string DirectoryNotFound = "The name of the directory is not valid!";
     }
 }
