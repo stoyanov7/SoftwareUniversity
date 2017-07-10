2faee6644e4ddce092a5adba57e933f398ab25ca
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-07-10 14:21:57 +0300

    Create SessonData class

---
+++ C#Development/BashSoft/BashSoft/StaticData/SessionData.cs
@@ -0,0 +1,9 @@
+﻿namespace BashSoft.StaticData
+{
+    using System.IO;
+    
+    public class SessionData
+    {
+        public static string currentPath = Directory.GetCurrentDirectory();
+    }
+}
