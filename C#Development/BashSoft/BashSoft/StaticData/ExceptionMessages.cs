bd073b4a6dd93275cb2c15d2c01c4a33519ea09b
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-07-10 14:22:24 +0300

    Move ExeptionMessages to StaticData folder

---
+++ C#Development/BashSoft/BashSoft/StaticData/ExceptionMessages.cs
@@ -0,0 +1,23 @@
+﻿namespace BashSoft.Exceptions
+{
+    public static class ExceptionMessages
+    {
+        public const string DataAlreadyInitialisedException = "Data is already initialized!";
+
+        public const string DataNotInitializedExceptionMessage = "The data structure must be initialised first in order to make any operations with it.";
+
+        public const string InexistingCourseInDataBase = "The course you are trying to get does not exist in the data base!";
+
+        public const string InexistingStudentInDataBase = "The user name for the student you are trying to get does not exist!";
+
+        public const string InvalidPath = "The name of the path is not valid!";
+
+        public const string ComparisonOfFilesWithDifferentSizes = "Files not of equal size, certain mismatch.";
+
+        public const string ForbiddenSymbolContainedInName = "Directory contains forbidden symbol in its name.";
+
+        public const string UnauthorizedAccessExceptionMessage = "The folder/file you are trying to get access needs a higher level of rights than you currently have.";
+
+        public static string UnableToGoHigherInPartitionHierarchy "Unable to go higher in partition hierarchy.";
+    }
+}
