bebfa0071d07886840ab0767cbfc63427065117a
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-07-11 14:32:20 +0300

    Create InputReader class

---
+++ C#Development/BashSoft/BashSoft/IO/InputReader.cs
@@ -0,0 +1,26 @@
+﻿namespace BashSoft.IO
+{
+    using BashSoft.StaticData;
+    using System;
+
+    public static class InputReader
+    {
+        private const string quitCommand = "quit";
+        private const string exitCommand = "exit";
+
+        public static void StartReadingCommands()
+        {
+            OutputWriter.WriteMessage($"{SessionData.currentPath}> ");
+            var input = Console.ReadLine();
+            input = input.Trim();
+
+            while (input != quitCommand && input != exitCommand)
+            {
+                CommandInterpreter.InterpredCommand(input);
+                OutputWriter.WriteMessage($"{SessionData.currentPath}> ");
+                input = Console.ReadLine();
+                input = input.Trim();
+            }
+        }
+    }
+}
