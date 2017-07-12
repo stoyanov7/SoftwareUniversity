39401eed087446ad3fed957af997c5239b2bc79c
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-07-12 17:08:30 +0300

    Create Manual String Processing - Lab

---
+++ C#Development/C#Advanced/ManualStringProcessing-Lab/01.StudentsResults/01.StudentsResults/StudentsResults.cs
@@ -0,0 +1,33 @@
+﻿namespace _01.StudentsResults
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StudentsResults
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var sb = new StringBuilder();
+            sb.AppendLine($"{"Name",-10}|{"CAdv",7}|{"COOP",7}|{"AdvOOP",7}|{"Average",7}|");
+
+            for (var i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine()
+                    .Split(new[] { ' ', ',', '-' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                var name = line[0];
+                var results = line
+                    .Skip(1)
+                    .Select(double.Parse)
+                    .ToList();
+
+                sb.AppendLine($"{name,-10}|{results[0],7:f2}|{results[1],7:f2}|{results[2],7:f2}|{results.Average(),7:f4}|");
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ C#Development/C#Advanced/ManualStringProcessing-Lab/05.ConcatenateStrings/05.ConcatenateStrings/ConcatenateStrings.cs
@@ -0,0 +1,21 @@
+﻿namespace _05.ConcatenateStrings
+{
+    using System;
+    using System.Text;
+
+    public class ConcatenateStrings
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var sb = new StringBuilder(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                sb.Append($"{Console.ReadLine()} ");
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
