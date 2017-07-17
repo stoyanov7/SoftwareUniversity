7a6ec04ee5a9c109b03d34a539a3915c6cf490e2
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-07-17 15:45:57 +0300

    Create LINQ - Lab

---
+++ C#Development/C#Advanced/LINQ-Lab/02.UpperStrings/02.UpperStrings/UpperStrings.cs
@@ -0,0 +1,19 @@
+﻿namespace _02.UpperStrings
+{
+    using System;
+    using System.Linq;
+
+    public class UpperStrings
+    {
+        public static void Main(string[] args)
+        {
+            var words = Console.ReadLine()
+                .Split(' ')
+                .Select(w => w.ToUpper())
+                .ToList();
+
+            words.ForEach(w => Console.Write(w + " "));
+                
+        }
+    }
+}
---
+++ C#Development/C#Advanced/LINQ-Lab/08.MapDistricts/08.MapDistricts/MapDistricts.cs
@@ -0,0 +1,51 @@
+﻿namespace _08.MapDistricts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MapDistricts
+    {
+        public static void Main(string[] args)
+        {
+            var citiesWithPopulation = new Dictionary<string, List<long>>();
+            var populations = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            var minimumPopulation = long.Parse(Console.ReadLine());
+
+            foreach (var item in populations)
+            {
+                var populationsTokens = item
+                    .Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                var name = populationsTokens.First();
+                var population = long.Parse(populationsTokens.Last());
+
+                if (!citiesWithPopulation.ContainsKey(name))
+                {
+                    citiesWithPopulation.Add(name, new List<long>());
+                }
+
+                citiesWithPopulation[name].Add(population);
+            }
+
+            citiesWithPopulation = 
+                citiesWithPopulation.Where(c => c.Value.Sum() >= minimumPopulation)
+                .OrderByDescending(c => c.Key)
+                .OrderByDescending(c => c.Value.Sum())
+                .ToDictionary(c => c.Key, c => c.Value);
+
+            foreach (var kvp in citiesWithPopulation)
+            {
+                var city = kvp.Key;
+                var population = kvp
+                    .Value
+                    .OrderByDescending(x => x)
+                    .Take(5);
+
+                Console.WriteLine($"{city}: {string.Join(" ", population)}");
+            }
+        }
+    }
+}
