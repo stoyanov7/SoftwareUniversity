5544e961781024e85a5f854c5ac3a3cd6280a000
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-07-21 12:44:50 +0300

    Using LINQ

--- C#Development/BashSoft/BashSoft/Repositories/RepositoryFilters.cs
+++ C#Development/BashSoft/BashSoft/Repositories/RepositoryFilters.cs
@@ -2,23 +2,24 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using IO;
     using StaticData;
 
-    public class RepositoryFilters
+    public static class RepositoryFilters
     {
         public static void FilterAndTake(Dictionary<string, List<int>> wantedData, string wantedFilter, int studentToTake)
         {
             switch (wantedFilter)
             {
                 case "excellent":
-                    FilterAndTake(wantedData, ExcellentFilter, studentToTake);
+                    FilterAndTake(wantedData, x => x >= 5.0, studentToTake);
                     break;
                 case "average":
-                    FilterAndTake(wantedData, AverageFilter, studentToTake);
+                    FilterAndTake(wantedData, x => x < 5.0 && x >= 3.5, studentToTake);
                     break;
                 case "poor":
-                    FilterAndTake(wantedData, PoorFilter, studentToTake);
+                    FilterAndTake(wantedData, x => x < 3.5, studentToTake);
                     break;
                 default:
                     OutputWriter.DisplayException(ExceptionMessages.InvalidStudentFilter);
@@ -26,46 +27,30 @@
             }
         }
 
-        private static void FilterAndTake(Dictionary<string, List<int>> wantedData, Predicate<double> givenFilter, int studentToTake)
+        private static void FilterAndTake(Dictionary<string, List<int>> wantedData, Predicate<double> givenFilter, int studentsToTake)
         {
-            var counterForPrintedStudents = 0;
+            var counterForPrinted = 0;
 
-            foreach (var usernamePoints in wantedData)
+            foreach (var userNamePoints in wantedData)
             {
-                if (counterForPrintedStudents == studentToTake)
+                if (counterForPrinted == studentsToTake)
                 {
                     break;
                 }
 
-                var averageMark = Average(usernamePoints.Value);
+                var averageScore = userNamePoints
+                    .Value
+                    .Average();
 
-                if (givenFilter(averageMark))
+                var percentageOfFullfillments = averageScore / 100;
+                var mark = percentageOfFullfillments * (4 + 2);
+
+                if (givenFilter(mark))
                 {
-                    OutputWriter.PrintStudent(usernamePoints);
-                    counterForPrintedStudents++;
+                    OutputWriter.PrintStudent(userNamePoints);
+                    counterForPrinted++;
                 }
             }
-        }
-
-        private static bool ExcellentFilter(double mark) => mark >= 5.0;
-
-        private static bool AverageFilter(double mark) => mark < 5.0 && mark >= 3.5;
-
-        private static bool PoorFilter(double mark) => mark < 3.5;
-
-        private static double Average(List<int> scoreOnTaks)
-        {
-            var totalScore = 0;
-
-            foreach (var score in scoreOnTaks)
-            {
-                totalScore += score;
-            }
-
-            var percentageOfAll = totalScore / (scoreOnTaks.Count * 100);
-            var averageMark = percentageOfAll * (4 + 2);
-
-            return averageMark;
         }
     }
 }
