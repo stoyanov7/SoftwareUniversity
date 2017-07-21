4cb062a4e667435617a5edf552cef659c7de470d
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-07-21 12:44:20 +0300

    Reduce code with LINQ and create PrintStudents

--- C#Development/BashSoft/BashSoft/Repositories/RepositorySorters.cs
+++ C#Development/BashSoft/BashSoft/Repositories/RepositorySorters.cs
@@ -1,11 +1,11 @@
 ﻿namespace BashSoft.Repositories
 {
-    using System;
     using System.Collections.Generic;
+    using System.Linq;
     using IO;
     using StaticData;
 
-    public class RepositorySorters
+    public static class RepositorySorters
     {
         public static void OrderAndTake(Dictionary<string, List<int>> wanterData, string comparison, int studentsToTake)
         {
@@ -14,10 +14,16 @@
             switch (comparison)
             {
                 case "ascending":
-                    OrderAndTake(wanterData, studentsToTake, CompareInOrder);
+                    PrintStudents(wanterData
+                        .OrderBy(x => x.Value.Sum())
+                        .Take(studentsToTake)
+                        .ToDictionary(x => x.Key, x => x.Value));
                     break;
                 case "descending":
-                    OrderAndTake(wanterData, studentsToTake, CompareInDesendingOrder);
+                    PrintStudents(wanterData
+                        .OrderByDescending(x => x.Value.Sum())
+                        .Take(studentsToTake)
+                        .ToDictionary(x => x.Key, x => x.Value));
                     break;
                 default:
                     OutputWriter.DisplayException(ExceptionMessages.InvalidComparisonQuery);
@@ -25,91 +31,12 @@
             }
         }
 
-        private static void OrderAndTake(Dictionary<string, List<int>> wantedData, int studentsToTake, Func<KeyValuePair<string, List<int>>, KeyValuePair<string, List<int>>, int> comparisonFunc)
+        private static void PrintStudents(Dictionary<string, List<int>> studentsSorted)
         {
-            // TODO: ???
-        }
-
-        private static int CompareInOrder(KeyValuePair<string, List<int>> firstValue, KeyValuePair<string, List<int>> secondValue)
-        {
-            var totalOfFirstMark = 0;
-
-            foreach (var i in firstValue.Value)
+            foreach (var kvp in studentsSorted)
             {
-                totalOfFirstMark += i;
+                OutputWriter.PrintStudent(kvp);
             }
-
-            var totalOfSecondMark = 0;
-
-            foreach (var i in secondValue.Value)
-            {
-                totalOfSecondMark += i;
-            }
-
-            return totalOfSecondMark.CompareTo(totalOfFirstMark);
-        }
-
-        private static int CompareInDesendingOrder(KeyValuePair<string, List<int>> firstValue, KeyValuePair<string, List<int>> secondValue)
-        {
-            var totalOfFirstMark = 0;
-
-            foreach (var i in firstValue.Value)
-            {
-                totalOfFirstMark += i;
-            }
-
-            var totalOfSecondMark = 0;
-
-            foreach (var i in secondValue.Value)
-            {
-                totalOfSecondMark += i;
-            }
-
-            return totalOfFirstMark.CompareTo(totalOfSecondMark);
-        }
-
-        private static Dictionary<string, List<int>> GetSortedStudents(Dictionary<string, List<int>> studentsWanted, int takeCount, Func<KeyValuePair<string, List<int>>, KeyValuePair<string, List<int>>, int> comaprison)
-        {
-            var valuesTaken = 0;
-            var studentsSorted = new Dictionary<string, List<int>>();
-            var nextInOrder = new KeyValuePair<string, List<int>>();
-            var isSorted = false;
-
-            while (valuesTaken < takeCount)
-            {
-                isSorted = true;
-
-                foreach (var studentsWithScore in studentsWanted)
-                {
-                    if (!string.IsNullOrEmpty(nextInOrder.Key))
-                    {
-                        var comparisonResult = comaprison(studentsWithScore, nextInOrder);
-
-                        if (comparisonResult >= 0 && !studentsSorted.ContainsKey(studentsWithScore.Key))
-                        {
-                            nextInOrder = studentsWithScore;
-                            isSorted = false;
-                        }
-                    }
-                    else
-                    {
-                        if (!studentsSorted.ContainsKey(studentsWithScore.Key))
-                        {
-                            nextInOrder = studentsWithScore;
-                            isSorted = true;
-                        }
-                    }
-                }
-
-                if (!isSorted)
-                {
-                    studentsSorted.Add(nextInOrder.Key, nextInOrder.Value);
-                    valuesTaken++;
-                    nextInOrder = new KeyValuePair<string, List<int>>();
-                }
-            }
-
-            return studentsSorted;
         }
     }
 }
