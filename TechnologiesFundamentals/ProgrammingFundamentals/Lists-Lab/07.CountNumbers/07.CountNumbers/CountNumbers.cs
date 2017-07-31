44a8d0b1aa974ef4db6cb190945d05b5da0df487
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-08-01 00:53:21 +0300

    Add 07. Count Numbers

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/Lists-Lab/07.CountNumbers/07.CountNumbers/CountNumbers.cs
@@ -0,0 +1,34 @@
+﻿namespace _07.CountNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CountNumbers
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var countNumbers = new SortedDictionary<int, int>();
+
+            foreach (var number in input)
+            {
+                if (!countNumbers.ContainsKey(number))
+                {
+                    countNumbers.Add(number, 0);
+                }
+
+                countNumbers[number]++;
+            }
+
+            foreach (var kvp in countNumbers)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
