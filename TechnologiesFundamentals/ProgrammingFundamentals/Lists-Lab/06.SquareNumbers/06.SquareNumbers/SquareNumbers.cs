ba4b05546ffc29cfe550177d834c1c4b3ac69af0
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-08-01 00:53:01 +0300

    Add 06. Square Numbers

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/Lists-Lab/06.SquareNumbers/06.SquareNumbers/SquareNumbers.cs
@@ -0,0 +1,34 @@
+﻿namespace _06.SquareNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SquareNumbers
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(new []{ ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            var squareNumbers = new List<int>();
+
+            foreach (var number in numbers)
+            {
+                var currentNum = Math.Sqrt(number);
+
+                if (currentNum == (int)currentNum)
+                {
+                    squareNumbers.Add(number);
+                }
+            }
+
+            squareNumbers.Sort();
+            squareNumbers.Reverse();
+
+            Console.WriteLine(string.Join(" ", squareNumbers));
+        }
+    }
+}
