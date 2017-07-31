7c601e9cb8bee6c4899b61331fc0263b68081dee
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2017-08-01 00:39:33 +0300

    Add RefactorSpecilaNumbers

---
+++ TechnologiesFundamentals/ProgrammingFundamentals/DataTypesAndVariables-Lab/09.RefactorSpecialNumbers/09.RefactorSpecialNumbers/RefactorSpecialNumbers.cs
@@ -0,0 +1,28 @@
+﻿namespace _09.RefactorSpecialNumbers
+{
+    using System;
+
+    public class RefactorSpecialNumbers
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (var i = 1; i <= n; i++)
+            {
+                var temporaryNumber = i;
+                var sum = 0;
+
+                while (temporaryNumber > 0)
+                {
+                    sum += temporaryNumber % 10;
+                    temporaryNumber /= 10;
+                }
+
+                var special = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine($"{i} -> {special}");
+                sum = 0;
+            }
+        }
+    }
+}
