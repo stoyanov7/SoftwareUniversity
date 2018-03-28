4ea9ddac1824362a4bd072305b595df6594441b4
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-03-28 11:14:52 +0300

    Add Iterators and Comparators - Lab

---
+++ C#Development/C#OOPAdvanced/IteratorsAndComparators-Lab/01.Library/Library/Library.cs
@@ -0,0 +1,41 @@
+﻿namespace Library
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Library : IEnumerable<Book>
+    {
+        private readonly SortedSet<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new SortedSet<Book>(books);
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+            private int index;
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                this.Reset();
+                this.books = new List<Book>(books);
+            }
+
+            public Book Current => this.books[this.index];
+
+            object IEnumerator.Current => this.Current;
+
+            public bool MoveNext() => ++this.index < this.books.Count;
+
+            public void Reset() => this.index = -1;
+
+            public void Dispose() { }
+        }
+
+        public IEnumerator<Book> GetEnumerator() => new LibraryIterator(this.books);
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
+}
---
+++ C#Development/C#OOPAdvanced/IteratorsAndComparators-Lab/01.Library/Library/Startup.cs
@@ -0,0 +1,10 @@
+﻿namespace Library
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
