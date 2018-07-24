8addd1f0de07f79b164f4afa21ddbfa94c6fb4a3
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-07-24 16:07:54 +0300

    Age, email and tag have validation attributes

---
+++ C#Development/Database/DatabasesAdvanced/BestPracticesAndArchitecture/PhotoShare.Utilities/ValidationAttributes/TagAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace PhotoShare.Utilities.ValidationAttributes
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text.RegularExpressions;
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    public class TagAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            const string pattern = "#[a-zA-Z0-9]{2,20}";
+            var regex = new Regex(pattern);
+            var result = regex.IsMatch(value.ToString());
+
+            return result;
+        }
+    }
+}
