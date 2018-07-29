f082692d4080d39c239b77da6ccc5ff3a8752ac2
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-07-29 14:20:43 +0300

    BusTicketsSystem Initial

---
+++ C#Development/Database/DatabasesAdvanced/BestPracticesAndArchitecture/BusTicketsSystem/BusTicketsSystem/Startup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BusTicketsSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
