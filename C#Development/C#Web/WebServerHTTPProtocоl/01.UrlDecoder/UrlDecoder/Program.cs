36ca641d0fc1efe42983212ef63c370b5c7b7c4b
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-08-01 16:24:38 +0300

    Initial C# Web

---
+++ C#Development/C#Web/WebServerHTTPProtocоl/01.UrlDecoder/UrlDecoder/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace UrlDecoder
+{
+    using System;
+    using System.Net;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var encodedUrl = Console.ReadLine();
+            var decodeUrl = WebUtility.UrlDecode(encodedUrl);
+
+            Console.WriteLine(decodeUrl);
+        }
+    }
+}
