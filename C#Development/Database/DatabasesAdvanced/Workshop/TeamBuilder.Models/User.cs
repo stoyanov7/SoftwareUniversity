71360c8b35953555c671bbc7f7bf58f1b5b7fc45
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-08-03 12:40:00 +0300

    Create Models

---
+++ C#Development/Database/DatabasesAdvanced/Workshop/TeamBuilder.Models/User.cs
@@ -0,0 +1,43 @@
+﻿namespace TeamBuilder.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using Enums;
+
+    public class User
+    {
+        public User()
+        {
+            this.CreatedEvents = new List<Event>();
+            this.UserTeams = new List<UserTeam>();
+            this.CreatedUserTeams = new List<UserTeam>();
+            this.ReceivedInvitations = new List<Invitation>();
+        }
+
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [MinLength(6)]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,30}$")]
+        public string Password { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public int Age { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public ICollection<Event> CreatedEvents { get; set; }
+
+        public ICollection<UserTeam> UserTeams { get; set; }
+
+        public ICollection<UserTeam> CreatedUserTeams { get; set; }
+
+        public ICollection<Invitation> ReceivedInvitations { get; set; }
+    }
+}
