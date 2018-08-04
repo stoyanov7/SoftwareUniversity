5cad49ba9e9e808fb3bac236181fb0c1b93925b2
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-08-04 17:22:55 +0300

    Implement Authentication manager

---
+++ C#Development/Database/DatabasesAdvanced/Workshop/TeamBuilder.Utilities/AuthenticationManager.cs
@@ -0,0 +1,38 @@
+﻿namespace TeamBuilder.Utilities
+{
+    using System;
+    using Models;
+
+    public static class AuthenticationManager
+    {
+        private static User currentUser;
+
+        public static void Authorize()
+        {
+            if (currentUser == null)
+            {
+                throw new InvalidOperationException("You should login first!");
+            }
+        }
+
+        public static User GetCurrentUser()
+        {
+            Authorize();
+            return currentUser;
+        }
+
+        public static bool IsAuthenticated() => currentUser != null;
+
+        public static void Login(User user)
+        {
+            currentUser = user ?? 
+                          throw new InvalidOperationException("You should logout first!");
+        }
+
+        public static void Logout()
+        {
+            Authorize();
+            currentUser = null;
+        }
+    }
+}
