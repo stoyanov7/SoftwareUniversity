8f8b3c187f8ac9afd750fcfe615ffc7cb0488fa4
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-08-04 17:25:31 +0300

    Add few of the commands

---
+++ C#Development/Database/DatabasesAdvanced/Workshop/TeamBuilder.Core/Commands/CreateEventCommand.cs
@@ -0,0 +1,49 @@
+﻿namespace TeamBuilder.Core.Commands
+{
+    using System;
+    using System.Globalization;
+    using Contracts;
+    using Utilities;
+    using Services.Contracts;
+
+    public class CreateEventCommand : ICommand
+    {
+        private readonly IEventService eventService;
+
+        public CreateEventCommand(IEventService eventService)
+        {
+            this.eventService = eventService;
+        }
+
+        public string Execute(string[] args)
+        {
+            Validator.CheckLength(6, args);
+            var currentUser = AuthenticationManager.GetCurrentUser();
+
+            var name = args[0];
+            var description = args[1];
+            var startDateString = $"{args[2]} {args[3]}";
+            var endDateString = $"{args[4]} {args[5]}";
+
+            var isValidStartDate = DateTime
+                .TryParseExact(startDateString, Constants.DateTimeFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out var startDate);
+
+            var isValidEndDate = DateTime
+                .TryParseExact(endDateString, Constants.DateTimeFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out var endDate);
+
+            if (!isValidStartDate || !isValidEndDate)
+            {
+                throw new ArgumentException(Constants.InvalidDateFormat);
+            }
+
+            if (startDate > endDate)
+            {
+                throw new ArgumentException(Constants.StartDate);
+            }
+
+            this.eventService.CreateEvent(name, description, startDate, endDate, currentUser.Id);
+
+            return string.Format(Constants.SuccessfullyCreatedEvent, name);
+        }
+    }
+}
---
+++ C#Development/Database/DatabasesAdvanced/Workshop/TeamBuilder.Core/Commands/CreateTeamCommand.cs
@@ -0,0 +1,55 @@
+﻿namespace TeamBuilder.Core.Commands
+{
+    using System;
+    using Contracts;
+    using Services.Contracts;
+    using Utilities;
+
+    public class CreateTeamCommand : ICommand
+    {
+        private readonly ITeamService teamService;
+        private readonly IUserTeamService userTeamService;
+
+        public CreateTeamCommand(ITeamService teamService, IUserTeamService userTeamService)
+        {
+            this.teamService = teamService;
+            this.userTeamService = userTeamService;
+        }
+
+        public string Execute(string[] args)
+        {
+            if (args.Length != 3 && args.Length != 2)
+            {
+                throw new InvalidOperationException(Constants.InvalidArgumentsCount);
+            }
+
+            var currentUser = AuthenticationManager.GetCurrentUser();
+
+            var name = args[0];
+            var acronym = args[1];
+            string description = null;
+
+            if (args.Length == 3)
+            {
+                description = args[2];
+            }
+
+            if (this.teamService.IsTeamExist(name))
+            {
+                throw new ArgumentException(string.Format(Constants.TeamExists, name));
+            }
+            
+            if (!this.teamService.IsAcronymValid(acronym))
+            {
+                throw new ArgumentException(string.Format(Constants.InvalidAcronym, acronym));
+            }
+            
+            this.teamService.CreateTeam(name, acronym, description, currentUser.Id);
+            var team = this.teamService.GetTeam(name);
+
+            this.userTeamService.CreateUserTeam(team.Id, currentUser.Id);
+
+            return string.Format(Constants.SuccessfullyCreatedTeam, name);
+        }
+    }
+}
