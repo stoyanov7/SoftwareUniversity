c80736b6f5417f55f16b0610bb76272124d4f17d
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-08-19 14:05:48 +0300

    Add new view models

--- C#Development/C#Web/C#MVCFrameworks-ASP.NETCore/BasicsAndRazorPages/MyLibrary.ViewModels/BookDetailsViewModel.cs
+++ C#Development/C#Web/C#MVCFrameworks-ASP.NETCore/BasicsAndRazorPages/MyLibrary.ViewModels/BookDetailsViewModel.cs
@@ -1,13 +1,16 @@
 ﻿namespace MyLibrary.ViewModels
 {
-    public class BookViewModel
+    public class BookDetailsViewModel
     {
         public int Id { get; set; }
 
         public string Title { get; set; }
 
-        public int AuthorId { get; set; }
         public string Author { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Description { get; set; }
 
         public string Status { get; set; }
     }
