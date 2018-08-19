520db24b7b71ec0ed4c2dcfa4e94c0a568512ab2
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-08-19 14:06:16 +0300

    Move ErrorViewModel to ViewModels class library

--- C#Development/C#Web/C#MVCFrameworks-ASP.NETCore/BasicsAndRazorPages/MyLibrary/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MyLibrary.Models.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(this.RequestId);
-    }
-}
