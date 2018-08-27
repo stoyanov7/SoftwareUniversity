3ba33716c824c6406b5fc2d0ebd8c4e4657167ed
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-08-27 13:05:46 +0300

    Add web server to the project

---
+++ C#Development/C#Web/C#WebDevelopmentBasics/StateManagement-Exercise/WebServer/Routing/Contracts/IServerRouteConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace WebServer.Routing.Contracts
+{
+    using Enums;
+    using System.Collections.Generic;
+
+    public interface IServerRouteConfig
+    {
+        IDictionary<HttpRequestMethod, IDictionary<string, IRoutingContext>> Routes { get; }
+    }
+}
