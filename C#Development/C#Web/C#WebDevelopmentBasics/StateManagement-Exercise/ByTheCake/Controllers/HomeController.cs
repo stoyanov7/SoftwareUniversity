f132451646381e85df6916c134f5bd5907623785
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-08-27 13:12:43 +0300

    Create Index page
    Implement all components for index page

---
+++ C#Development/C#Web/C#WebDevelopmentBasics/StateManagement-Exercise/ByTheCake/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿namespace ByTheCake.Controllers
+{
+    using Views.Home;
+    using WebServer.Enums;
+    using WebServer.Http.Contracts;
+    using WebServer.Http.Response;
+
+    public class HomeController
+    {
+        public IHttpResponse Index()
+        {
+            return new ViewResponse(HttpStatusCode.Ok, new Index());
+        }
+    }
+}
