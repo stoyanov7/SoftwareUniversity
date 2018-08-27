809c840cada30fa5a8ba642af8623e1878f4fd88
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-08-27 13:08:55 +0300

    Configure the server and routes for the server

---
+++ C#Development/C#Web/C#WebDevelopmentBasics/StateManagement-Exercise/ByTheCake/ByTheCakeApp.cs
@@ -0,0 +1,16 @@
+﻿namespace ByTheCake
+{
+    using Controllers;
+    using WebServer.Contracts;
+    using WebServer.Routing.Contracts;
+
+    public class ByTheCakeApp : IApplication
+    {
+        public void Configure(IAppRouteConfig appRouteConfig)
+        {
+            appRouteConfig.Get("/", req => new HomeController().Index());
+
+            appRouteConfig.Get("Cake/Add", req => new CakeController().Add());
+        }
+    }
+}
---
+++ C#Development/C#Web/C#WebDevelopmentBasics/StateManagement-Exercise/ByTheCake/Startup.cs
@@ -0,0 +1,26 @@
+﻿namespace ByTheCake
+{
+    using WebServer;
+    using WebServer.Contracts;
+    using WebServer.Routing;
+
+    public class Startup : IRunnable
+    {
+        public static void Main(string[] args)
+        {
+            new Startup().Run();
+        }
+
+        public void Run()
+        {
+            var mainApplication = new ByTheCakeApp();
+
+            var appRouteConfig = new AppRouteConfig();
+            mainApplication.Configure(appRouteConfig);
+
+            var webServer = new WebServer(7777, appRouteConfig);
+
+            webServer.Run();
+        }
+    }
+}
