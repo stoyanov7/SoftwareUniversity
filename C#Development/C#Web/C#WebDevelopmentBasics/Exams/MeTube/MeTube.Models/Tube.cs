2b7c2e0d06c51910d62d5d2ac2704b8cb96c7cbe
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-08-28 15:43:06 +0300

    Implement models

---
+++ C#Development/C#Web/C#WebDevelopmentBasics/Exams/MeTube/MeTube.Models/Tube.cs
@@ -0,0 +1,29 @@
+﻿namespace MeTube.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Tube
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public string YouTubeId { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Views { get; set; }
+
+        [Required]
+        public int UploaderId { get; set; }
+        public User Uploader { get; set; }
+    }
+}
---
+++ C#Development/C#Web/C#WebDevelopmentBasics/Exams/MeTube/MeTube.Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MeTube.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        public string Username { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public ICollection<Tube> Tubes { get; set; }
+    }
+}
