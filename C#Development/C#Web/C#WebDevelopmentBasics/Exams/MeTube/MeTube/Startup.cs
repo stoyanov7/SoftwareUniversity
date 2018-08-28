763fbef607172598b0aad805086f637ed511081c
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-08-28 15:43:54 +0300

    Initial web server and run mvc framework

---
+++ C#Development/C#Web/C#WebDevelopmentBasics/Exams/MeTube/MeTube/Startup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MeTube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
