7e277ba91334200409be3fa9e1917527be181583
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-08-28 15:43:28 +0300

    Configure the data structure and tables

---
+++ C#Development/C#Web/C#WebDevelopmentBasics/Exams/MeTube/MeTube.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace MeTube.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString = "Data Source=.;Database=MeTube;Integrated Security=True";
+    }
+}
---
+++ C#Development/C#Web/C#WebDevelopmentBasics/Exams/MeTube/MeTube.Data/MeTubeContext.cs
@@ -0,0 +1,35 @@
+﻿namespace MeTube.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class MeTubeContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Tube> Tubes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Username)
+                .IsUnique();
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
