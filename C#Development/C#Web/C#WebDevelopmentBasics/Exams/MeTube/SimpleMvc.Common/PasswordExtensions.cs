ee816776b9b374ce764351f2a3cfc444efb414c1
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-08-29 12:33:39 +0300

    Add password hash method

---
+++ C#Development/C#Web/C#WebDevelopmentBasics/Exams/MeTube/SimpleMvc.Common/PasswordExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace SimpleMvc.Common
+{
+    using System.Collections.Generic;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    public static class PasswordExtensions
+    {
+        public static string GenerateSHA256String(string inputString)
+        {
+            var sha256 = SHA256.Create();
+            var bytes = Encoding.UTF8.GetBytes(inputString);
+            var hash = sha256.ComputeHash(bytes);
+
+            return GetStringFromHash(hash);
+        }
+
+        private static string GetStringFromHash(IEnumerable<byte> hash)
+        {
+            var result = new StringBuilder();
+
+            foreach (var i in hash)
+            {
+                result.Append(i.ToString("X2"));
+            }
+
+            return result.ToString();
+        }
+    }
+}
