31b1ea517c7be333da8b6ae62fe3db899791a262
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-08-30 16:11:47 +0300

    User can upload video

---
+++ C#Development/C#Web/C#WebDevelopmentBasics/Exams/MeTube/MeTube/Models/BindingModels/TubeUploadBindingModel.cs
@@ -0,0 +1,21 @@
+﻿namespace MeTube.Models.BindingModels
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class TubeUploadBindingModel
+    {
+        [Required]
+        [MinLength(3)]
+        public string Title { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        public string Author { get; set; }
+
+        [Required]
+        [DataType(DataType.Url)]
+        public string YoutubeLink { get; set; }
+
+        public string Description { get; set; }
+    }
+}
