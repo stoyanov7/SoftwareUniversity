ce7434056efc21fa4a0aa2ebc754cdc38403fcdd
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-08-31 15:00:28 +0300

    Index page show data for guests and another for users

---
+++ C#Development/C#Web/C#WebDevelopmentBasics/Exams/MeTube/MeTube/Models/ViewModels/TubeProfileViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace MeTube.Models.ViewModels
+{
+    using System;
+
+    public class TubeProfileViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public static Func<Tube, TubeProfileViewModel> FromTube =>
+            tube => new TubeProfileViewModel
+            {
+                Id = tube.Id,
+                Title = tube.Title,
+                Author = tube.Author
+            };
+    }
+}
