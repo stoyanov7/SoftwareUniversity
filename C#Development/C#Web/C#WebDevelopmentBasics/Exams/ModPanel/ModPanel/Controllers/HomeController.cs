ecc4cc4cfd8165a5dfa34232fd2cb048cb976e50
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-09-21 10:51:01 +0300

    Project can be run with home page

---
+++ C#Development/C#Web/C#WebDevelopmentBasics/Exams/ModPanel/ModPanel/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿namespace ModPanel.Controllers
+{
+    using SimpleMvc.Framework.Attributes.Methods;
+    using SimpleMvc.Framework.Controllers;
+    using SimpleMvc.Framework.Interfaces;
+
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index() => this.View();
+    }
+}
